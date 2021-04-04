c0d819b41bac11904ecbbbdb7bce0f405786a450
Miguel de Benito Delgado <debenito@unternehmertum.de>
2021-04-04 21:34:03 +0200

    [WIP] Naive, untested exact implementation

---
+++ src/valuation/shapley/exact.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from functools import lru_cache
+from itertools import permutations
+from collections import OrderedDict
+from valuation.reporting.scores import sort_values
+from valuation.utils import Dataset, SupervisedModel
+
+
+def combinatorial_shapley(model: SupervisedModel, data: Dataset) -> OrderedDict:
+    """ Computes the exact Shapley value. """
+
+    # Arbitrary choice ~= 1.14 hours if 1 sec per fit() + score()
+    if len(data) > 12:
+        raise ValueError(f"Large dataset! Computation requires {2**len(data)} "
+                         f"calls to model.fit()")
+
+    # There is a clever way of rearranging the loops to fit just once per
+    # list of indices. Or... we can just cache and be done with it.
+    @lru_cache
+    def utility(indices):
+        x = data.x_train[data.index.isin(indices)]
+        y = data.y_train[data.index.isin(indices)]
+        model.fit(x, y)
+        return model.score(data.x_test, data.y_test)
+
+    values = np.zeros(len(data))
+    for p in permutations(data.index):
+        for i in range(len(p)):
+            values[p[i]] += utility(p[:i+1]) - utility(p[:i])
+    values /= np.math.factorial(len(data))
+
+    return sort_values({i: v for i, v in enumerate(values)})
