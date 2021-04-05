cfe8081cecd1788345d50dab17bb40ae10fd35b9
Miguel de Benito Delgado <debenito@unternehmertum.de>
2021-04-05 11:42:36 +0200

    Notebook tests don't work

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,30 +1,31 @@
 import logging
 import os
-
 import nbformat
 import pytest
+
 from nbconvert.preprocessors import ExecutePreprocessor
 
-NOTEBOOKS_DIR = "notebooks"
-DOCS_DIR = "docs"
+NOTEBOOKS_DIR = "../notebooks"
+DOCS_DIR = "../docs"
 resources = {"metadata": {"path": NOTEBOOKS_DIR}}
 
 log = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize(
-    "notebook", [file for file in os.listdir(NOTEBOOKS_DIR) if file.endswith(".ipynb")]
-)
+    "notebook",
+    [file for file in os.listdir(NOTEBOOKS_DIR) if file.endswith(".ipynb")])
 def test_notebook(notebook):
     notebook_path = os.path.join(NOTEBOOKS_DIR, notebook)
     log.info(f"Reading jupyter notebook from {notebook_path}")
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
     ep = ExecutePreprocessor(timeout=600)
+    # FIXME: seems to be an outdated implementation wrt nbconvert 6.0
     with ep.setup_preprocessor(nb, resources=resources):
         for i, cell in enumerate(nb["cells"]):
             log.info(f"processing cell {i} from {notebook}")
-            ep.preprocess_cell(cell, resources=resources, cell_index=i)
+            ep.preprocess_cell(cell, resources=resources, index=i)
 
     # saving the executed notebook to docs
     output_path = os.path.join(DOCS_DIR, notebook)
