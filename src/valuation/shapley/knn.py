be1719e0eec8525f51ab508558514f7b7062b3d1
Miguel de Benito Delgado <debenito@unternehmertum.de>
2021-04-05 13:27:44 +0200

    [skip ci] in progress...

--- src/valuation/shapley/knn.py
+++ src/valuation/shapley/knn.py
@@ -1,12 +1,13 @@
 from collections import OrderedDict
+from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+from tqdm.auto import tqdm
 from valuation.reporting.scores import sort_values
 from valuation.utils import Dataset
 
-from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
-
 
 def exact_knn_shapley(data: Dataset,
-                      model: KNeighborsClassifier) -> OrderedDict:
+                      model: KNeighborsClassifier,
+                      progress: bool=True) -> OrderedDict:
     """ Computes exact Shapley values for a KNN classifier or regressor.py
      :param data: split Dataset
      :param model: model to extract parameters from. The object will not be
@@ -16,10 +17,13 @@ def exact_knn_shapley(data: Dataset,
                 'metric': 'minkowski',
                 'p': 2}
     defaults.update(model.get_params())
+    # HACK: NearestNeighbors doesn't support this. There will be more...
+    del defaults['weights']
     n_neighbors = defaults['n_neighbors']  # This must be set!
     defaults['n_neighbors'] = len(data)  # We want all training points sorted
 
-    assert n_neighbors > len(data)
+    assert n_neighbors < len(data)
+    # assert data.target_dim == 1
 
     nns = NearestNeighbors(**defaults).fit(data.x_train)
     # closest to farthest
@@ -29,16 +33,18 @@ def exact_knn_shapley(data: Dataset,
     values = {i: 0.0 for i in data.index}
     n = len(data)
     yt = data.y_train
-    for j, y, _, ii in enumerate(zip(data.y_test, distances, indices)):
-        value_at_x = int(yt[ii[-1]] == y) / n
+    iterator = enumerate(zip(data.y_test.values, distances, indices), start=1)
+    iterator = tqdm(iterator) if progress else iterator
+    for j, (y, _, ii) in iterator:
+        value_at_x = int(yt.iloc[ii[-1]] == y) / n
         values[ii[-1]] = (1/j)*value_at_x + (j-1/j)*values[ii[-1]]
-        for i in range(n-2, n_neighbors, step=-1):  # farthest to closest
+        for i in range(n-2, n_neighbors, -1):  # farthest to closest
             value_at_x = values[ii[i+1]] \
-                + (int(yt[ii[i]] == y) - int(yt[ii[i+1]] == y)) / i
+                + (int(yt.iloc[ii[i]] == y) - int(yt.iloc[ii[i+1]] == y)) / i
             values[ii[i]] = (1/j)*value_at_x + (j-1/j)*values[ii[i]]
-        for i in range(n_neighbors, -1, step=-1):  # farthest to closest
+        for i in range(n_neighbors, -1, -1):  # farthest to closest
             value_at_x = values[ii[i+1]] \
-                + (int(yt[ii[i]] == y) - int(yt[ii[i + 1]] == y)) / n_neighbors
+                + (int(yt.iloc[ii[i]] == y) - int(yt.iloc[ii[i + 1]] == y)) / n_neighbors
             values[ii[i]] = (1/j)*value_at_x + (j-1/j)*values[ii[i]]
 
     return sort_values(values)
