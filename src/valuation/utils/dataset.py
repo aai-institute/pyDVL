d01144295a3ce2900dba05f4a7bc8e0649432429
Miguel de Benito Delgado <debenito@unternehmertum.de>
2021-04-05 18:52:51 +0200

    Save target names

--- src/valuation/utils/dataset.py
+++ src/valuation/utils/dataset.py
@@ -5,15 +5,21 @@ from sklearn.model_selection import train_test_split
 
 class Dataset:
     """ Meh... """
-    def __init__(self, data: Bunch, train_size: float = 0.8,
+    def __init__(self, data: Bunch,
+                 train_size: float = 0.8,
                  random_state: int = None):
         x_train, x_test, y_train, y_test = \
             train_test_split(data.data, data.target,
                              train_size=train_size, random_state=random_state)
+        try:
+            target_names = data.target_names
+        except AttributeError:
+            target_names = ['target']
+
         self.x_train = pd.DataFrame(x_train, columns=data.feature_names)
-        self.y_train = pd.DataFrame(y_train, columns=['target'])
+        self.y_train = pd.DataFrame(y_train, columns=target_names)
         self.x_test = pd.DataFrame(x_test, columns=data.feature_names)
-        self.y_test = pd.DataFrame(y_test, columns=['target'])
+        self.y_test = pd.DataFrame(y_test, columns=target_names)
         self._description = data.DESCR
 
         assert (self.x_train.index == self.y_train.index).all(), "huh?"
