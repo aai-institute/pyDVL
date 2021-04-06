3ce6971f32f9578aa6ed96a848c19bca527f26f5
Miguel de Benito Delgado <debenito@unternehmertum.de>
2021-04-06 22:14:30 +0200

    Cosmetic

--- tests/valuation/conftest.py
+++ tests/valuation/conftest.py
@@ -15,9 +15,7 @@ def linear_dataset():
     a = 2
     b = 0
     x = np.arange(-1, 1, .2)
-    # x = np.array([-1., -0.6, -0.2, 0.7, 1.0])
     y = np.random.normal(loc=a * x + b, scale=1)
-    # y = a*x + b
     db = Bunch()
     db.data, db.target = x.reshape(-1, 1), y.reshape(-1, 1)
     db.DESCR = f"y~{a}*x + {b}"
