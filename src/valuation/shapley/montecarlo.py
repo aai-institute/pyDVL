7d1a075424a83d4e22039f93f60ee0a5e38d1c20
Miguel de Benito Delgado <debenito@unternehmertum.de>
2021-08-13 17:18:42 +0200

    [skip ci] Fumbling around with combinatorial mcs (does not work)

--- src/valuation/shapley/montecarlo.py
+++ src/valuation/shapley/montecarlo.py
@@ -40,11 +40,10 @@ class ShapleyWorker(InterruptibleWorker):
                  ):
         """
         :param u: Utility object with model, data, and scoring function
-        :param global_score:
+        :param global_score: Score of the model on an independent test set
         :param score_tolerance: For every permutation, computation stops
-         after the mean increase in performance is within score_tolerance*stddev
-         of the bootstrapped score over the **test** set (i.e. we bootstrap to
-         compute variance of scores, then use that to stop)
+         after the mean increase in performance is within score_tolerance of
+         global_score
         :param min_scores: Use so many of the last samples for a permutation
          in order to compute the moving average of scores.
         :param progress: set to True to display progress bars
@@ -110,8 +109,8 @@ def truncated_montecarlo_shapley(u: Utility,
     the moving average for all values falls below another threshold.
 
         :param u: Utility object with model, data, and scoring function
-        :param bootstrap_iterations: Repeat global_score computation this many
-         times to estimate variance.
+        :param bootstrap_iterations: Repeat the computation of `global_score`
+         this many times to estimate its variance.
         :param min_scores: Use so many of the last scores in order to compute 
          the moving average.
         :param score_tolerance: For every permutation, computation stops
@@ -170,7 +169,7 @@ def truncated_montecarlo_shapley(u: Utility,
         iteration += 1
 
         converged = vanishing_derivatives(values, min_values=min_values,
-                                          eps=value_tolerance)
+                                          atol=value_tolerance)
         converged_history.append(converged)
         # converged can decrease. reset() clears times, but if just call
         # update(), the bar collapses. This is some hackery to fix that:
@@ -254,7 +253,7 @@ def serial_truncated_montecarlo_shapley(u: Utility,
 
         converged = \
             vanishing_derivatives(all_marginals, min_values=min_steps,
-                                  eps=value_tolerance)
+                                  atol=value_tolerance)
         converged_history.append(converged)
         pbar2.update(converged)
         if converged >= n:
@@ -276,8 +275,6 @@ def permutation_montecarlo_shapley(u: Utility,
     """
     FIXME: the sum of values tends to cancel out, even though the ranking is ok
     """
-    n = len(u.data)
-
     def fun(job_id: int):
         n = len(u.data)
         values = np.zeros(n).reshape((-1, 1))
@@ -297,10 +294,11 @@ def permutation_montecarlo_shapley(u: Utility,
     backend = make_nested_backend('loky')()
     results = Parallel(n_jobs=num_jobs, backend=backend) \
         (delayed(fun)(job_id=j+1) for j in range(num_jobs))
-    acc = np.row_stack(results).sum(axis=1) / (max_iterations*num_jobs)
+    acc = np.row_stack(results).sum(axis=0) / (max_iterations*num_jobs)
     return sort_values({i: v for i, v in enumerate(acc)}), None
 
 
+# FIXME: This is completely broken. Normalization is in a weird way. (??!?)
 def combinatorial_montecarlo_shapley(u: Utility,
                                      max_iterations: int,
                                      num_jobs: int = 1,
@@ -312,17 +310,19 @@ def combinatorial_montecarlo_shapley(u: Utility,
     FIXME: this seems to have very high variance! It might help to implement
      importance sampling with more weight for smaller set sizes, where value
       contributions are likely to be higher.
+      UPDATE: Seems dubious, since the uniform sampling strategy does not help
     """
     n = len(u.data)
+
+    # TODO: make this a parameter
+    dist = PowerSetDistribution.WEIGHTED
 
     def fun(indices: np.ndarray, job_id: int) -> np.ndarray:
         values = np.zeros(len(indices))
         for i, idx in enumerate(indices):
             # Randomly sample subsets of full dataset without idx
             subset = np.setxor1d(u.data.indices, [idx], assume_unique=True)
-            power_set = random_powerset(subset,
-                                        dist=PowerSetDistribution.UNIFORM,
-                                        max_subsets=max_iterations)
+            power_set = random_powerset(subset, dist=dist, max_subsets=max_iterations)
             # Normalization accounts for a uniform dist. on powerset (i.e. not
             # weighted by set size) and the montecarlo sampling
             for s in maybe_progress(power_set, progress, desc=f"Index {idx}",
@@ -330,7 +330,8 @@ def combinatorial_montecarlo_shapley(u: Utility,
                 values[i] += (u({idx}.union(s)) - u(s)) \
                              / np.math.comb(n-1, len(s))
 
-        return 2**(n-2) * values / (n * max_iterations)
+        correction = 1 if dist == PowerSetDistribution.WEIGHTED else 2**(n-1)/n
+        return correction * values / max_iterations
 
     job = MapReduceJob.from_fun(fun, np.concatenate)
     results = map_reduce(job, u.data.indices, num_jobs=num_jobs)[0]
