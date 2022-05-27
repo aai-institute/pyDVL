e09bf2062ffe04c885548d7e6db548b48aefc287
Markus Semmler <markus.semmler@unternehmertum.de>
2022-05-27 09:24:51 -0500

    Modify types.py

--- src/valuation/utils/types.py
+++ src/valuation/utils/types.py
@@ -2,11 +2,12 @@ from typing import Callable, Protocol, TypeVar
 from numpy import ndarray
 from torch import tensor
 
-__all__ = ['SupervisedModel', 'Scorer', 'unpackable']
+__all__ = ["SupervisedModel", "Scorer", "unpackable"]
 
 
 class SupervisedModel(Protocol):
-    """ Pedantic: only here for the type hints. """
+    """Pedantic: only here for the type hints."""
+
     def fit(self, x: ndarray, y: ndarray):
         pass
 
@@ -20,35 +21,23 @@ class SupervisedModel(Protocol):
 # ScorerNames = Literal[very long list here]
 # instead... ScorerNames = str
 
-Scorer = TypeVar('Scorer',
-                 str, Callable[[SupervisedModel, ndarray, ndarray], float])
-
-
-class TorchObjective(Protocol):
-
-    def __call__(self, x: tensor, y: tensor, **kwargs) -> tensor:
-        pass
-
-
-class BatchInfluenceFunction(Protocol):
-
-    def __call__(self, v: ndarray) -> ndarray:
-        pass
+Scorer = TypeVar("Scorer", str, Callable[[SupervisedModel, ndarray, ndarray], float])
 
 
 def unpackable(cls: type) -> type:
-    """ A class decorator that allows unpacking of all attributes of an object
-     with the double asterisk operator. E.g.
+    """A class decorator that allows unpacking of all attributes of an object
+    with the double asterisk operator. E.g.
 
-        @unpackable
-        @dataclass
-        class Schtuff:
-            a: int
-            b: str
+       @unpackable
+       @dataclass
+       class Schtuff:
+           a: int
+           b: str
 
-        x = Schtuff(a=1, b='meh')
-        d = dict(**x)
+       x = Schtuff(a=1, b='meh')
+       d = dict(**x)
     """
+
     def keys(self):
         return self.__dict__.keys()
 
@@ -71,11 +60,23 @@ def unpackable(cls: type) -> type:
         for k in self.keys():
             yield k, getattr(self, k)
 
-    setattr(cls, 'keys', keys)
-    setattr(cls, '__getitem__', __getitem__)
-    setattr(cls, '__len__', __len__)
-    setattr(cls, '__iter__', __iter__)
-    setattr(cls, 'update', update)
-    setattr(cls, 'items', items)
+    setattr(cls, "keys", keys)
+    setattr(cls, "__getitem__", __getitem__)
+    setattr(cls, "__len__", __len__)
+    setattr(cls, "__iter__", __iter__)
+    setattr(cls, "update", update)
+    setattr(cls, "items", items)
 
     return cls
+
+
+class TorchObjective(Protocol):
+
+    def __call__(self, x: tensor, y: tensor, **kwargs) -> tensor:
+        pass
+
+
+class BatchInfluenceFunction(Protocol):
+
+    def __call__(self, v: ndarray) -> ndarray:
+        pass
