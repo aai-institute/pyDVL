b79d0baccfe6e6a99db787dacabf317a76831278
Anes Benmerzoug <anes.benmerzoug@unternehmertum.de>
2022-05-30 17:26:05 +0200

    Lint setup.py and test files

--- tests/utils/test_dataset.py
+++ tests/utils/test_dataset.py
@@ -1,6 +1,6 @@
 """ FIXME: more to do... """
 
+
 def test_dataset_len(boston_dataset):
     assert len(boston_dataset) == len(boston_dataset.x_train) == 404
     assert len(boston_dataset.x_train) + len(boston_dataset.x_test) == 506
-
