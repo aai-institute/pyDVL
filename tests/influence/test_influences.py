d179a9aaa3d6c387c949de7d4d7395fde2b84cdd
Markus Semmler <markus.semmler@unternehmertum.de>
2022-06-01 22:45:00 -0500

    Modify test_influences function

--- tests/influence/test_influences.py
+++ tests/influence/test_influences.py
@@ -1,72 +1,43 @@
-from dataclasses import field
-from functools import partial
+import itertools
 
 import numpy as np
 import pytest
 import torch.nn.functional as F
-from sklearn.model_selection import train_test_split
 
 from valuation.influence.naive import influences
 from valuation.models.linear_regression_torch_model import LRTorchModel
 from valuation.models.neural_network_torch_model import NNTorchModel
 from valuation.models.pytorch_model import PyTorchSupervisedModel
-from valuation.utils import Utility, Dataset
-
-
-def create_random_dataset(
-    n_in_features: int = 3, n_out_features: int = 1, n_samples: int = 1000
-):
-    x = np.random.normal(0, 0.1, size=[n_samples, n_in_features])
-    A = np.random.uniform(-1, 1.0, size=[n_out_features, n_in_features])
-    y = x @ A.T
-    y = y.ravel()
-    return x, y
-
-
-all_data = []
-# all_data += [
-#     create_random_dataset(i, n_samples=1000)
-#     + (
-#         LRTorchModel,
-#         j,
-#     )
-#     for i in range(1, 5)
-#     for j in [1, 2]
-# ]
-all_data += [
-    create_random_dataset(i, n_samples=1000)
-    + (
-        partial(NNTorchModel, n_neurons_per_Layer=[4]),
-        1,
-    )
-    for i in range(1, 5)
-]
+from valuation.utils import Dataset
 
 
 @pytest.mark.parametrize(
-    "x,y,torch_model_factory,n_jobs",
-    all_data,
+    "torch_model_factory,n_jobs",
+    list(
+        itertools.product(
+            [
+                lambda *args: NNTorchModel(*args, n_neurons_per_Layer=[8, 8]),
+                LRTorchModel,
+            ],
+            [1, 2],
+        )
+    ),
 )
 def test_influences(
-    x: np.ndarray,
-    y: np.ndarray,
+    linear_dataset: Dataset,
     torch_model_factory,
     n_jobs: int,
-    train_size: int = 0.8,
 ):
-    n_in_features = x.shape[1]
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y, random_state=0, train_size=train_size
-    )
-    dataset = Dataset(x_train, y_train, x_test, y_test)
+    n_in_features = linear_dataset.x_test.shape[1]
 
-    objective = F.mse_loss
     model = PyTorchSupervisedModel(
-        torch_model_factory(n_in_features, 1), objective, num_epochs=10
+        model=torch_model_factory(n_in_features, 1), objective=F.mse_loss, num_epochs=10
     )
-    model.fit(dataset.x_train, dataset.y_train)
-    utility = Utility(model, dataset, objective)
-    influence_values = influences(utility, progress=True, n_jobs=n_jobs)
+    model.fit(linear_dataset.x_train, linear_dataset.y_train)
+    influence_values = influences(model, linear_dataset, progress=True, n_jobs=n_jobs)
 
     assert np.all(np.logical_not(np.isnan(influence_values)))
-    assert influence_values.shape == (len(x_test), len(x_train))
+    assert influence_values.shape == (
+        len(linear_dataset.x_test),
+        len(linear_dataset.x_train),
+    )
