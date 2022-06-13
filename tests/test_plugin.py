d4afa57d4da60c780ef0e84cb2a6e835a3675ddc
Anes Benmerzoug <anes.benmerzoug@unternehmertum.de>
2022-06-13 12:01:59 +0200

    Update tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -3,45 +3,50 @@ import pytest
 
 @pytest.mark.tolerate(max_failures=1)
 @pytest.mark.parametrize("i", range(1))
-def test_marker_with_parametrize(i):
+def test_marker_only(i):
     assert False
 
 
-@pytest.fixture(scope="function", params=range(2))
-def data(request):
-    yield
-
-
-@pytest.mark.xfail
-@pytest.mark.tolerate(max_failures=1)
-def test_marker_with_data_fixture(data):
-    assert False
-
-
-@pytest.mark.tolerate(max_failures=1)
-@pytest.mark.parametrize(
-    "i", [range(1), pytest.param(range(2), marks=pytest.mark.xfail)]
+@pytest.fixture(
+    scope="function", params=[0, pytest.param(1, marks=pytest.mark.xfail), 2]
 )
-def test_marker_and_fixture(tolerate, i):
-    for _ in i:
-        with tolerate:
-            assert False
+def data(request):
+    yield request.param
 
 
-def test_fixture(tolerate):
-    for _ in range(1):
+@pytest.mark.tolerate(max_failures=1)
+def test_marker_only_with_data_fixture(data):
+    assert False
+
+
+@pytest.mark.parametrize("i", [1, pytest.param(2, marks=pytest.mark.xfail)])
+def test_fixture_only(tolerate, i):
+    for _ in range(i):
         with tolerate(max_failures=1):
             assert False
 
 
-@pytest.mark.xfail
+@pytest.mark.xfail(
+    reason="This should fail because we should pass arguments when calling the tolerate fixture"
+)
 def test_fixture_call_no_arguments(tolerate):
     for _ in range(1):
         with tolerate():
             assert False
 
 
-@pytest.mark.xfail()
+@pytest.mark.tolerate(max_failures=1)
+@pytest.mark.parametrize("i", [1, pytest.param(2, marks=pytest.mark.xfail)])
+def test_marker_and_fixture(tolerate, i):
+    for _ in range(i):
+        with tolerate:
+            assert False
+
+
+@pytest.mark.xfail(
+    reason="This should fail because the tolerate marker expects arguments",
+    raises=ValueError,
+)
 @pytest.mark.tolerate()
 def test_failure():
     pass
