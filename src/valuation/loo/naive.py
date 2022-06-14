e5e8dc38432c6f88c376789e80be95176fee1248
Anes Benmerzoug <anes.benmerzoug@unternehmertum.de>
2022-06-14 09:42:06 +0200

    Fix bug in naive loo implementation

--- src/valuation/loo/naive.py
+++ src/valuation/loo/naive.py
@@ -7,13 +7,14 @@ from valuation.utils import Dataset, SupervisedModel, maybe_progress, utility
 
 
 def naive_loo(
-    model: SupervisedModel, data: Dataset, progress: bool = True
+    model: SupervisedModel, data: Dataset, progress: bool = True, **kwargs
 ) -> OrderedDict[int, float]:
     """Computes the LOO score for each training point in the dataset."""
-    u = lambda x: utility(model, data, frozenset(x))
+    u = utility.Utility(model, data, **kwargs)
+    compute_utility = lambda x: u(frozenset(x))
     values = {i: 0.0 for i in data.indices}
     for i in maybe_progress(data.indices, progress):
         subset = np.setxor1d(data.indices, [i], assume_unique=True)
-        values[i] = u(data.indices) - u(subset)
+        values[i] = compute_utility(data.indices) - compute_utility(subset)
 
     return sort_values(values)
