514c3d0c0845b461698caf1ceddbc48cf0b3bd6a
Xuzzo <f.xuzzo@gmail.com>
2022-06-14 18:10:11 +0200

    remove xgboost

--- tests/shapley/test_montecarlo.py
+++ tests/shapley/test_montecarlo.py
@@ -3,7 +3,6 @@ from functools import partial
 
 import numpy as np
 import pytest
-import xgboost as xgb
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 
