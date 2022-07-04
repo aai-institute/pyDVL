34a269c14a7f1db37aafa52c8c202c1fb11985f6
Markus Semmler <markus.semmler@unternehmertum.de>
2022-07-04 12:42:18 -0500

    Update the influences.ipynb notebook and refactor some methods.

--- src/valuation/utils/dataset.py
+++ src/valuation/utils/dataset.py
@@ -1,3 +1,4 @@
+from copy import copy
 from typing import List
 
 import numpy as np
@@ -153,3 +154,21 @@ def polynomial_dataset(coefficients: np.ndarray):
     db.feature_names = ["x"]
     db.target_names = ["y"]
     return Dataset.from_sklearn(data=db, train_size=0.5), coefficients
+
+
+def flip_dataset(
+    dataset: Dataset, flip_percentage: float, in_place: bool = False
+) -> Tuple[Dataset, np.ndarray]:
+    """
+    Takes a binary classification problem and inverts a certain percentage of the labels.
+
+    :param dataset: A binary classification problem.
+    :param flip_percentage: A float between [0, 1] describing how much labels shall be flipped.
+    :param in_place: True, if the old dataset should be not copied but used by value as reference.
+    :returns: A dataset differing in 5% of the labels to the orignal one.
+    """
+    flipped_dataset = copy(dataset) if not in_place else dataset
+    flip_num_samples = int(flip_percentage * len(dataset.x_train))
+    idx = np.random.choice(len(dataset.x_train), replace=False, size=flip_num_samples)
+    flipped_dataset.y_train[idx] = 1 - flipped_dataset.y_train[idx]
+    return flipped_dataset, idx
