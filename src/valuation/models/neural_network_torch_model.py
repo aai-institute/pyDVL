2c96b29024faed107e46edb99eeb575c8d64cbdb
Markus Semmler <markus.semmler@unternehmertum.de>
2022-07-05 12:53:45 -0500

    Added first version for neural network data.

--- src/valuation/models/neural_network_torch_model.py
+++ src/valuation/models/neural_network_torch_model.py
@@ -2,7 +2,7 @@ from typing import List
 
 import torch
 import torch.nn as nn
-from torch.nn import ReLU
+from torch.nn import ReLU, Tanh
 
 
 class NNTorchModel(nn.Module):
@@ -14,20 +14,21 @@ class NNTorchModel(nn.Module):
 
         all_dimensions = [self.n_input] + self.n_hidden_layers + [self.n_output]
         layers = []
+        num_layers = len(all_dimensions) - 1
         for num_layer, (in_features, out_features) in enumerate(
             zip(all_dimensions[:-1], all_dimensions[1:])
         ):
             linear_layer = nn.Linear(
                 in_features, out_features, bias=num_layer < len(all_dimensions) - 2
             )
-            torch.nn.init.xavier_uniform(linear_layer.weight)
+            torch.nn.init.xavier_uniform_(linear_layer.weight)
 
             if num_layer < len(all_dimensions) - 2:
                 linear_layer.bias.data.fill_(0.01)
 
             layers.append(linear_layer)
-            if num_layer == len(all_dimensions) - 2:
-                layers.append(ReLU())
+            if num_layer < num_layers - 1:
+                layers.append(Tanh())
 
         self.layers = nn.Sequential(*layers)
 
--- src/valuation/utils/plotting.py
+++ src/valuation/utils/plotting.py
@@ -6,10 +6,11 @@ import numpy as np
 
 def plot_datasets(
     datasets: Dict[str, Tuple[np.ndarray, np.ndarray]],
-    x_min: np.ndarray,
-    x_max: np.ndarray,
-    line: np.ndarray,
+    x_min: np.ndarray = None,
+    x_max: np.ndarray = None,
+    line: np.ndarray = None,
     colors: Dict[str, np.ndarray] = None,
+    s: float = None,
 ):
 
     has_custom_colors = colors is not None
@@ -19,17 +20,24 @@ def plot_datasets(
     if has_custom_colors:
         v_max = max([np.max(v) for k, v in colors.items()])
 
+    if num_datasets == 1:
+        ax = [ax]
+
     for i, dataset_name in enumerate(datasets.keys()):
         x, y = datasets[dataset_name]
         ax[i].set_title(dataset_name)
-        ax[i].set_xlim(x_min[0], x_max[0])
-        ax[i].set_ylim(x_min[1], x_max[1])
-        ax[i].plot(line[:, 0], line[:, 1], color="black")
+        if x_min is not None:
+            ax[i].set_xlim(x_min[0], x_max[0])
+        if x_max is not None:
+            ax[i].set_ylim(x_min[1], x_max[1])
+
+        if line is not None:
+            ax[i].plot(line[:, 0], line[:, 1], color="black")
 
         if not has_custom_colors:
             for v in np.unique(y):
                 idx = np.argwhere(y == v)
-                ax[i].scatter(x[idx, 0], x[idx, 1], label=str(v))
+                ax[i].scatter(x[idx, 0], x[idx, 1], label=str(v), s=s)
         else:
             points = ax[i].scatter(
                 x[:, 0],
@@ -38,6 +46,7 @@ def plot_datasets(
                 vmin=0,
                 vmax=v_max,
                 cmap="plasma",
+                s=s,
             )
 
     if has_custom_colors:
