0f37a7d97591c0ba0a306f4b66824adac98c838d
Xuzzo <f.xuzzo@gmail.com>
2022-09-14 16:38:31 +0200

    fix damn pytorch import

--- tests/influence/test_influences.py
+++ tests/influence/test_influences.py
@@ -8,11 +8,6 @@ from sklearn.datasets import load_wine
 from sklearn.preprocessing import MinMaxScaler
 
 from tests.conftest import create_mock_dataset
-from valuation.influence.linear import (
-    influences_perturbation_linear_regression_analytical,
-    influences_up_linear_regression_analytical,
-    linear_influences,
-)
 from valuation.utils import Dataset
 
 try:
@@ -21,6 +16,11 @@ try:
     from torch.optim import Adam, lr_scheduler
 
     from valuation.influence.general import influences
+    from valuation.influence.linear import (
+        influences_perturbation_linear_regression_analytical,
+        influences_up_linear_regression_analytical,
+        linear_influences,
+    )
     from valuation.influence.model_wrappers import (
         TorchLinearRegression,
         TorchNeuralNetwork,
