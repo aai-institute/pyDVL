5d04652ac79e0efc5f38e24736c7c71d3a6525d6
Anes Benmerzoug <anes.benmerzoug@unternehmertum.de>
2022-09-20 15:47:54 +0200

    Remove redundant __all__ variable in utils package

--- src/valuation/utils/__init__.py
+++ src/valuation/utils/__init__.py
@@ -7,27 +7,3 @@ from valuation.utils.plotting import *
 from valuation.utils.progress import *
 from valuation.utils.types import *
 from valuation.utils.utility import *
-
-__all__ = [
-    "memcached",
-    "SupervisedModel",
-    "Dataset",
-    "Scorer",
-    "map_reduce",
-    "MapReduceJob",
-    "available_cpus",
-    "unpackable",
-    "Utility",
-    "serialize",
-    "powerset",
-    "maybe_progress",
-    "linear_regression_analytical_derivative_d_theta",
-    "linear_regression_analytical_derivative_d_x_d_theta",
-    "linear_regression_analytical_derivative_d2_theta",
-    "load_spotify_dataset",
-    "plot_shapley",
-    "mcmc_is_linear_function",
-    "mcmc_is_linear_function_positive_definite",
-    "MatrixVectorProduct",
-    "get_running_avg_variance",
-]
