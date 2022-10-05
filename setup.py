a2aaf2ae5808c6ffa105ba765f5fe2dac60e88b3
Anes Benmerzoug <anes.benmerzoug@unternehmertum.de>
2022-10-05 13:42:24 +0200

    Simplify extra requirements

--- setup.py
+++ setup.py
@@ -6,32 +6,6 @@ from setuptools import find_packages, setup
 repository_root = Path(__file__).parent
 long_description = (repository_root / "README.md").read_text()
 
-test_requirements = ["pytest"]
-
-
-# this function should be put in 'setup.py'
-def get_extra_requires(path, add_all=True):
-    import re
-    from collections import defaultdict
-
-    with open(path) as fp:
-        extra_deps = defaultdict(set)
-        for k in fp:
-            if k.strip() and not k.startswith("#"):
-                tags = set()
-                if ":" in k:
-                    k, v = k.split(":")
-                    tags.update(vv.strip() for vv in v.split(","))
-                tags.add(re.split("[<=>]", k)[0])
-                for t in tags:
-                    extra_deps[t].add(k)
-
-        # add tag `all` at the end
-        if add_all:
-            extra_deps["all"] = set(vv for v in extra_deps.values() for vv in v)
-
-    return extra_deps
-
 
 setup(
     name="pyDVL",
@@ -46,8 +20,10 @@ setup(
         if not line.startswith("--")
     ],
     setup_requires=["wheel"],
-    tests_require=test_requirements,
-    extras_require=get_extra_requires("requirements-extra.txt"),
+    tests_require=["pytest"],
+    extras_require={
+        "influence": ["torch"],
+    },
     author="appliedAI",
     long_description=long_description,
     long_description_content_type="text/markdown",
