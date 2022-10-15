1b9ae653b435b36c345e9e812bb19c1eac57b2bd
Miguel de Benito Delgado <debenito@unternehmertum.de>
2022-10-15 21:39:34 +0200

    Don't copy notebooks if it isn't necessary

--- docs/_ext/copy_notebooks.py
+++ docs/_ext/copy_notebooks.py
@@ -15,13 +15,13 @@ def copy_notebooks(app: Sphinx, config: Config) -> None:
     notebooks_dir = root_dir / "notebooks"
     docs_examples_dir = root_dir / "docs" / "examples"
     notebook_filepaths = list(notebooks_dir.glob("*.ipynb"))
-    sep = "\n\t"
-    logger.info(
-        f"Found the following notebooks: "
-        f"{sep.join([str(p) for p in notebook_filepaths])}"
-    )
     for notebook in notebook_filepaths:
         target_filepath = docs_examples_dir / notebook.name
+        if os.path.getmtime(notebook) <= os.path.getmtime(target_filepath):
+            logger.info(
+                f"Notebook '{os.fspath(notebook)}' hasn't been updated, skipping."
+            )
+            continue
         logger.info(
             f"Copying '{os.fspath(notebook)}' to '{os.fspath(target_filepath)}'"
         )
