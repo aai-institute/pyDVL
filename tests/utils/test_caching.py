cb90a9ddd76e5e21a081c8bee2d81ab880b9a497
Miguel de Benito Delgado <m.debenito@appliedai.de>
2022-12-03 08:26:26 +0100

    Type fixes (np.sum() does not operate on Iterables)

--- tests/utils/test_caching.py
+++ tests/utils/test_caching.py
@@ -45,7 +45,7 @@ def test_memcached_single_job(memcached_client):
 
     # TODO: maybe this should be a fixture too...
     @memcached(client_config=config, time_threshold=0)  # Always cache results
-    def foo(indices: Iterable[int]) -> float:
+    def foo(indices: "NDArray[int]") -> float:
         return float(np.sum(indices))
 
     n = 1000
@@ -66,7 +66,7 @@ def test_memcached_parallel_jobs(memcached_client):
         # Note that we typically do NOT want to ignore run_id
         ignore_args=["job_id", "run_id"],
     )
-    def foo(indices: Iterable[int], *args, **kwargs) -> float:
+    def foo(indices: "NDArray[int]", *args, **kwargs) -> float:
         # logger.info(f"run_id: {run_id}, running...")
         return float(np.sum(indices))
 
@@ -95,7 +95,7 @@ def test_memcached_repeated_training(memcached_client):
         # Note that we typically do NOT want to ignore run_id
         ignore_args=["job_id", "run_id"],
     )
-    def foo(indices: Iterable[int]) -> float:
+    def foo(indices: "NDArray[int]") -> float:
         # from pydvl.utils.logging import logger
         # logger.info(f"run_id: {run_id}, running...")
         return float(np.sum(indices)) + np.random.normal(scale=10)
@@ -120,13 +120,13 @@ def test_memcached_faster_with_repeated_training(memcached_client):
         # Note that we typically do NOT want to ignore run_id
         ignore_args=["job_id", "run_id"],
     )
-    def foo_cache(indices: Iterable[int]) -> float:
+    def foo_cache(indices: "NDArray[int]") -> float:
         # from pydvl.utils.logging import logger
         # logger.info(f"run_id: {run_id}, running...")
         sleep(0.01)
         return float(np.sum(indices)) + np.random.normal(scale=1)
 
-    def foo_no_cache(indices: Iterable[int]) -> float:
+    def foo_no_cache(indices: "NDArray[int]") -> float:
         # from pydvl.utils.logging import logger
         # logger.info(f"run_id: {run_id}, running...")
         sleep(0.01)
@@ -177,12 +177,12 @@ def test_memcached_parallel_repeated_training(
         # Note that we typically do NOT want to ignore run_id
         ignore_args=["job_id", "run_id"],
     )
-    def map_func(indices: Iterable[int]) -> float:
+    def map_func(indices: "NDArray[int]") -> float:
         # from pydvl.utils.logging import logger
         # logger.info(f"run_id: {run_id}, running...")
         return np.sum(indices).item() + np.random.normal(scale=10)
 
-    def reduce_func(chunks: Iterable[float]) -> float:
+    def reduce_func(chunks: "NDArray[float]") -> float:
         return np.sum(chunks).item()
 
     map_reduce_job = MapReduceJob(map_func, reduce_func, n_jobs=n_jobs, n_runs=n_runs)
