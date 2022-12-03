429fa6fe6a45e02ff6168f18446f3a8d73272f6c
Miguel de Benito Delgado <m.debenito@appliedai.de>
2022-12-03 10:27:50 +0100

    Test indexing of ValuationResult. Simplify with fixture

--- tests/test_results.py
+++ tests/test_results.py
@@ -9,12 +9,9 @@ def dummy_valuator():
     pass
 
 
-@pytest.mark.parametrize(
-    "values, names, ranks_asc",
-    [([], [], []), ([2, 3, 1], ["a", "b", "c"], [2, 0, 1])],
-)
-def test_sorting(values, names, ranks_asc):
-    v = ValuationResult(
+@pytest.fixture
+def dummy_values(values, names):
+    return ValuationResult(
         dummy_valuator,
         status=ValuationStatus.Converged,
         values=np.array(values),
@@ -23,39 +20,38 @@ def test_sorting(values, names, ranks_asc):
         sort=SortOrder.Ascending,
     )
 
-    assert np.alltrue([it.value for it in v] == sorted(values))
-    assert np.alltrue(v.indices == ranks_asc)
 
-    v.sort(SortOrder.Descending)
-    assert np.alltrue([it.value for it in v] == sorted(values, reverse=True))
-    assert np.alltrue(v.indices == list(reversed(ranks_asc)))
+@pytest.mark.parametrize(
+    "values, names, ranks_asc",
+    [([], [], []), ([2, 3, 1], ["a", "b", "c"], [2, 0, 1])],
+)
+def test_sorting(values, names, ranks_asc, dummy_values):
+
+    assert np.alltrue([it.value for it in dummy_values] == sorted(values))
+    assert np.alltrue(dummy_values.indices == ranks_asc)
+
+    dummy_values.sort(SortOrder.Descending)
+    assert np.alltrue([it.value for it in dummy_values] == sorted(values, reverse=True))
+    assert np.alltrue(dummy_values.indices == list(reversed(ranks_asc)))
 
 
 @pytest.mark.parametrize(
     "values, names, ranks_asc",
     [([], [], []), ([2, 3, 1], ["a", "b", "c"], [2, 0, 1])],
 )
-def test_dataframe_sorting(values, names, ranks_asc):
-    v = ValuationResult(
-        dummy_valuator,
-        status=ValuationStatus.Converged,
-        values=np.array(values),
-        stderr=np.zeros_like(values),
-        data_names=names,
-        sort=SortOrder.Ascending,
-    )
+def test_dataframe_sorting(values, names, ranks_asc, dummy_values):
     sorted_names = [names[r] for r in ranks_asc]
     try:
         import pandas
 
-        df = v.to_dataframe(use_names=False)
+        df = dummy_values.to_dataframe(use_names=False)
         assert np.alltrue(df.index.values == ranks_asc)
 
-        df = v.to_dataframe(use_names=True)
+        df = dummy_values.to_dataframe(use_names=True)
         assert np.alltrue(df.index.values == sorted_names)
         assert np.alltrue(df["dummy_valuator"].values == sorted(values))
 
-        df = v.sort(SortOrder.Descending).to_dataframe(use_names=True)
+        df = dummy_values.sort(SortOrder.Descending).to_dataframe(use_names=True)
         assert np.alltrue(df.index.values == list(reversed(sorted_names)))
         assert np.alltrue(df["dummy_valuator"].values == sorted(values, reverse=True))
     except ImportError:
@@ -67,20 +63,11 @@ def test_dataframe_sorting(values, names, ranks_asc):
     "values, names, ranks_asc",
     [([], [], []), ([2, 3, 1], ["a", "b", "c"], [2, 0, 1])],
 )
-def test_iter(values, names, ranks_asc):
-    values = ValuationResult(
-        dummy_valuator,
-        status=ValuationStatus.Converged,
-        values=np.array(values),
-        stderr=np.zeros_like(values),
-        data_names=names,
-        sort=SortOrder.Ascending,
-    )
-
-    for rank, it in enumerate(values):
+def test_iter(names, ranks_asc, dummy_values):
+    for rank, it in enumerate(dummy_values):
         assert it.index == ranks_asc[rank]
 
-    for rank, it in enumerate(values):
+    for rank, it in enumerate(dummy_values):
         assert it.name == names[ranks_asc[rank]]
 
 
@@ -88,21 +75,34 @@ def test_iter(values, names, ranks_asc):
     "values, names, ranks_asc",
     [([], [], []), ([2, 3, 1], ["a", "b", "c"], [2, 0, 1])],
 )
-def test_todataframe(values, names, ranks_asc):
-    values = ValuationResult(
-        dummy_valuator,
-        status=ValuationStatus.Converged,
-        values=np.array(values),
-        stderr=np.zeros_like(values),
-        data_names=names,
-        sort=SortOrder.Ascending,
-    )
-    df = values.to_dataframe()
+def test_todataframe(ranks_asc, dummy_values):
+    df = dummy_values.to_dataframe()
     assert "dummy_valuator" in df.columns
     assert "dummy_valuator_stderr" in df.columns
     assert np.alltrue(df.index.values == ranks_asc)
 
-    df = values.to_dataframe(column="val")
+    df = dummy_values.to_dataframe(column="val")
     assert "val" in df.columns
     assert "val_stderr" in df.columns
     assert np.alltrue(df.index.values == ranks_asc)
+
+
+@pytest.mark.parametrize(
+    "values, names, ranks_asc",
+    [([], [], []), ([2.0, 3.0, 1.0, 6.0], ["a", "b", "c", "d"], [2, 0, 1, 3])],
+)
+def test_indexing(ranks_asc, dummy_values):
+    dummy_values.sort(SortOrder.Ascending)
+    if len(ranks_asc) == 0:
+        with pytest.raises(IndexError):
+            dummy_values[1]
+        dummy_values[:2]
+    else:
+        assert ranks_asc[:] == [it.index for it in dummy_values[:]]
+        assert ranks_asc[0] == dummy_values[0].index
+        assert [ranks_asc[0]] == [it.index for it in dummy_values[[0]]]
+        assert ranks_asc[:2] == [it.index for it in dummy_values[:2]]
+        assert ranks_asc[:2] == [it.index for it in dummy_values[[0, 1]]]
+        assert ranks_asc[:-2] == [it.index for it in dummy_values[:-2]]
+        assert ranks_asc[-2:] == [it.index for it in dummy_values[-2:]]
+        assert ranks_asc[-2:] == [it.index for it in dummy_values[[-2, -1]]]
