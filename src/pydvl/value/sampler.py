04f7271868562ff740996e9dd6c5e4d2ba7cb824
Miguel de Benito Delgado <m.debenito@appliedai.de>
2022-12-17 13:14:03 -0500

    Make max samples a stopping criterion. Samplers don't terminate. Other related fixes

--- src/pydvl/value/sampler.py
+++ src/pydvl/value/sampler.py
@@ -1,14 +1,15 @@
 from enum import Enum
 from typing import (
+    TYPE_CHECKING,
     Any,
     Generator,
     Generic,
     Optional,
     Sequence,
-    TYPE_CHECKING,
     Tuple,
     TypeVar,
 )
+
 import numpy as np
 
 from pydvl.utils import powerset, random_powerset
@@ -22,8 +23,8 @@ T = TypeVar("T")
 class Sampler(Generic[T]):
     """Samplers iterate over subsets.
 
-    For each index, the complement set indices is considered, and at most
-    ``max_subsets`` from the power set are generated.
+    For each element in the whole set, the complementary set is considered, and
+    at most ``max_subsets`` from its power set are generated.
 
 
     .. example::
@@ -38,35 +39,62 @@ class Sampler(Generic[T]):
 
     """
 
-    def __init__(self, indices: "NDArray[T]", max_subsets: Optional[int] = None):
+    class IndexIteration(Enum):
+        Sequential = "sequential"
+        Random = "random"
+
+    def __init__(
+        self,
+        indices: "NDArray[T]",
+        index_iteration: IndexIteration = IndexIteration.Sequential,
+    ):
+        """
+        :param indices: The set of items (indices) to sample from.
+        """
         self._indices = indices
-        self._max_subsets = max_subsets
+        self._index_iteration = index_iteration
 
     def __iter__(self):
         raise NotImplementedError()
 
-    def complement(self, exclude: Sequence[T], *args, **kwargs) -> Sequence[T]:
+    def complement(self, exclude: Sequence[T], *args, **kwargs) -> "NDArray[T]":
         return np.setxor1d(self._indices, exclude)
+
+    def indices(self) -> Generator[T, Any, None]:
+        """
+        FIXME: this is probably not very useful, but I couldn't decide
+          which method is better
+        """
+        if self._index_iteration is Sampler.IndexIteration.Sequential:
+            for idx in self._indices:
+                yield idx
+        elif self._index_iteration is Sampler.IndexIteration.Random:
+            while True:
+                yield np.random.choice(self._indices, size=1).item()
 
 
 class DeterministicSampler(Sampler[T]):
+    def __init__(self, indices: "NDArray[T]"):
+        """
+        :param indices: The set of items (indices) to sample from.
+        """
+        super().__init__(indices, Sampler.IndexIteration.Sequential)
+
     def __iter__(self) -> Generator[T, Any, None]:
-        for idx in self._indices:
+        for idx in self.indices():
             for subset in powerset(self.complement(idx)):
                 yield idx, subset
 
 
 class UniformSampler(Sampler[T]):
     def __iter__(self) -> Generator[Tuple[T], Any, None]:
-        for idx in self._indices:
-            for subset in random_powerset(
-                self.complement([idx]), max_subsets=self._max_subsets
-            ):
-                yield idx, subset
+        while True:
+            for idx in self.indices():
+                for subset in random_powerset(self.complement([idx]), max_subsets=1):
+                    yield idx, subset
 
 
 class AntitheticSampler(Sampler[T]):
-    # FIXME: overload with different args
     def complement(
         self, exclude: Sequence[T], exclude_idx: Optional[T] = None, *args, **kwargs
     ) -> "NDArray[np.int_]":
@@ -76,12 +104,11 @@ class AntitheticSampler(Sampler[T]):
         return np.setxor1d(tmp, [exclude_idx])
 
     def __iter__(self) -> Generator[Tuple[T], Any, None]:
-        for idx in self._indices:
-            for subset in random_powerset(
-                self.complement([idx]), max_subsets=self._max_subsets
-            ):
-                yield idx, subset
-                yield idx, self.complement(subset, idx)
+        while True:
+            for idx in self.indices():
+                for subset in random_powerset(self.complement([idx]), max_subsets=1):
+                    yield idx, subset
+                    yield idx, self.complement(subset, idx)
 
 
 class PermutationSampler(Sampler[T]):
@@ -92,50 +119,44 @@ class PermutationSampler(Sampler[T]):
     """
 
     def __iter__(self) -> Generator[Tuple[T], Any, None]:
-        permutation = np.random.permutation(self._indices)
-        for i, idx in enumerate(permutation):
-            yield idx, permutation[:i]
+        while True:
+            permutation = np.random.permutation(self._indices)
+            for i, idx in enumerate(permutation):
+                yield idx, permutation[:i]
 
 
 class HierarchicalSampler(Sampler[T]):
     def __iter__(self) -> Generator[Tuple[T], Any, None]:
-        for idx in self._indices:
-            for k in range(len(self._indices)):
-                for subset in random_powerset(
-                    self.complement([idx]),
-                    size=k,  # FIXME: not implemented
-                    max_subsets=1 + self._max_subsets // k,
+        while True:
+            for idx in self.indices():
+                k = np.random.choice(np.arange(len(self._indices)), size=1).item()
+                for subset in random_powerset(  # FIXME: not implemented
+                    self.complement([idx]), size=k, max_subsets=1
                 ):
                     yield idx, subset
 
 
-class OwenSampler(Generic[T]):
+class OwenSampler(Sampler[T]):
     class Algorithm(Enum):
         Standard = "standard"
         Antithetic = "antithetic"
 
-    def __init__(
-        self,
-        indices: "NDArray[T]",
-        method: Algorithm,
-        n_steps: int,
-        max_subsets: Optional[int] = None,
-    ):
+    def __init__(self, indices: "NDArray[T]", method: Algorithm, n_steps: int):
+        super().__init__(indices)
         q_stop = {
             OwenSampler.Algorithm.Standard: 1.0,
             OwenSampler.Algorithm.Antithetic: 0.5,
         }
         self.q_steps = np.linspace(start=0, stop=q_stop[method], num=n_steps)
-        self._indices = indices
-        self._max_subsets = max_subsets
 
-    def complement(self, exclude: Sequence[T]):
+    def complement(self, exclude: Sequence[T], *args, **kwargs):
         return np.setxor1d(self._indices, exclude)
 
     def __iter__(self) -> Generator[Tuple[np.float, T, T], Any, None]:
-        for idx in self._indices:
-            for j, q in enumerate(self.q_steps):
-                for subset in random_powerset(
-                    self.complement([idx]), q=q, max_subsets=self._max_subsets
-                ):
-                    yield q, idx, subset
+        while True:
+            for idx in self.indices():
+                for j, q in enumerate(self.q_steps):
+                    for subset in random_powerset(
+                        self.complement([idx]), q=q, max_subsets=1
+                    ):
+                        yield q, idx, subset
--- src/pydvl/value/shapley/semivalues.py
+++ src/pydvl/value/shapley/semivalues.py
@@ -1,35 +1,46 @@
 import math
 from functools import lru_cache
-from typing import Callable, Optional
+from typing import TYPE_CHECKING, Callable, Protocol
 
 import numpy as np
-from numpy._typing import NDArray
 
 from pydvl.utils import Utility, get_running_avg_variance, maybe_progress
 from pydvl.value import ValuationResult, ValuationStatus
 from pydvl.value.sampler import OwenSampler, PermutationSampler, Sampler, UniformSampler
 
+if TYPE_CHECKING:
+    from numpy._typing import NDArray
 
-SemiValue = Callable[[Utility, Sampler, ...], ValuationResult]
-Coefficient = Callable[[int, int], int]
+
+Coefficient = Callable[[int, int], float]
 StoppingCriterion = Callable[
-    [int, NDArray[np.float_], NDArray[np.float_], NDArray[np.float_]], ValuationStatus
+    [int, "NDArray[np.float_]", "NDArray[np.float_]", "NDArray[np.int_]"],
+    ValuationStatus,
 ]
 
 
+class SemiValue(Protocol):
+    def __call__(
+        self, u: Utility, stop: StoppingCriterion, *args, **kwargs
+    ) -> ValuationResult:
+        ...
+
+
 def _semivalues(
-    sampler: Sampler,
     u: Utility,
-    coefficient: Optional[Coefficient] = None,
-    stop: Optional[StoppingCriterion] = None,
+    sampler: Sampler,
+    coefficient: Coefficient,
+    stop: StoppingCriterion,
     *,
     progress: bool = False,
     job_id: int = 1,
 ) -> ValuationResult:
-    """Helper function sent to workers for all computations of semi-values.
+    """Helper function used for all computations of semi-values.
 
     :param u: Utility object with model, data, and scoring function.
     :param sampler: The subset sampler to use for utility computations.
+    :param coefficient: The semivalue coefficient
+
     :param progress: Whether to display progress bars for each job.
     :param job_id: id to use for reporting progress.
     :return: Object with valuation results.
@@ -38,23 +49,18 @@ def _semivalues(
     values = np.zeros(n, dtype=np.float_)
     variances = np.zeros(n, dtype=np.float_)
     counts = np.zeros(n, dtype=np.int_)
-    if coefficient is None:
-        coefficient = lambda _, __: 1.0  # noqa
-        coefficient.__name__ = "none"
     status = ValuationStatus.Pending
-    for idx, s in maybe_progress(sampler, progress, position=job_id):
+
+    for step, (idx, s) in maybe_progress(enumerate(sampler), progress, position=job_id):
         marginal = (u({idx}.union(s)) - u(s)) * coefficient(n, len(s))
         values[idx], variances[idx] = get_running_avg_variance(
             values[idx], variances[idx], marginal, counts[idx]
         )
         counts[idx] += 1
-        if stop is not None:
-            status = stop(values, variances, counts)
-            if status != ValuationStatus.Pending:
-                break
+        status = stop(step, values, variances, counts)
+        if status != ValuationStatus.Pending:
+            break
 
-    if stop is None:
-        status = ValuationStatus.MaxIterations
     return ValuationResult(
         algorithm=f"semivalue_{coefficient.__name__}_{sampler.__class__.__name__}",
         status=status,
@@ -63,73 +69,111 @@ def _semivalues(
     )
 
 
-def variance_criterion(
-    values: NDArray[np.float_],
-    variances: NDArray[np.float_],
-    counts: NDArray[np.int_],
-    eps: float,
-    values_ratio: float,
-) -> ValuationStatus:
-    r"""Checks that the variance of the values is below a threshold.
+def max_samples_criterion(max_samples: np.int_) -> StoppingCriterion:
+    def check_max_samples(
+        step: int,
+        values: "NDArray[np.float_]",
+        variances: "NDArray[np.float_]",
+        counts: "NDArray[np.int_]",
+    ) -> ValuationStatus:
+        if step >= max_samples:
+            return ValuationStatus.MaxIterations
+        return ValuationStatus.Pending
 
-    A value $v_i$ is considered to be below the threshold if the associated
-    standard deviation $s_i = \sqrt{\var{v_ii}}$ fulfills:
+    return check_max_samples
 
-    $$ s_i \lt | \eps v_i | . $$
 
-    In other words, the computation of the value for sample $x_i$ is considered
-    complete once the square root of its variance is within a fraction $\eps$ of
-    the value.
-
-    .. fixme::
-       This ad-hoc criterion might fail under some circumstances. Complement it
-       or find something better.
-
-    :param values: The array of data values.
-    :param variances: The array of variances for each value.
-    :param counts: The array counting the number of updates performed so far
-        to each value.
-    :param eps: Threshold multiplier for the values
+def max_updates_criterion(max_updates: int, values_ratio: float) -> StoppingCriterion:
+    """Checks whether a given fraction of all values has been updated at
+    least ``maxiter`` times.
+    :param max_updates: Maximal amount of updates for each value
     :param values_ratio: Amount of values that must fulfill the criterion
     :return: :attr:`~pydvl.value.results.ValuationStatus.Converged` if at least
         a fraction of `values_ratio` of the values has standard error below the
         threshold.
 
     """
-    if len(values) == 0:
-        raise ValueError("Empty values array")
-    if len(values) != len(variances) or len(values) != len(counts):
-        raise ValueError("Mismatching array lengths")
-    if (
-        not isinstance(values, np.ndarray)
-        or not isinstance(variances, np.ndarray)
-        or not isinstance(counts, np.ndarray)
-    ):
-        raise TypeError("Input must be NDArray")
 
-    sat_ratio = np.count_nonzero(variances <= np.abs(eps * values)) / len(values)
-    if sat_ratio >= values_ratio:
-        return ValuationStatus.Converged
-    return ValuationStatus.Pending
+    def check_max_updates(
+        step: int,
+        values: "NDArray[np.float_]",
+        variances: "NDArray[np.float_]",
+        counts: "NDArray[np.int_]",
+    ) -> ValuationStatus:
+        if np.count_nonzero(counts >= max_updates) / len(counts) >= values_ratio:
+            return ValuationStatus.MaxIterations
+        return ValuationStatus.Pending
+
+    return check_max_updates
+
+
+def stderr_criterion(eps: float, values_ratio: float) -> StoppingCriterion:
+    r"""Checks that the standard error of the values is below a threshold.
+
+    A value $v_i$ is considered to be below the threshold if the associated
+    standard error $s_i = \sqrt{\var{v_ii}/n}$ fulfills:
+
+    $$ s_i \lt | \eps v_i | . $$
+
+    In other words, the computation of the value for sample $x_i$ is considered
+    complete once the estimator for the standard error is within a fraction
+    $\eps$ of the value.
+
+    .. fixme::
+       This ad-hoc will fail if the distribution of utilities for an index has
+       high variance. We need something better, taking 1st or maybe 2nd order
+       info into account.
+
+    :param eps: Threshold multiplier for the values
+    :param values_ratio: Amount of values that must fulfill the criterion
+    :return: A convergence criterion returning
+        :attr:`~pydvl.value.results.ValuationStatus.Converged` if at least a
+        fraction of `values_ratio` of the values has standard error below the
+        threshold.
+    """
+
+    def check_stderr(
+        step: int,
+        values: "NDArray[np.float_]",
+        variances: "NDArray[np.float_]",
+        counts: "NDArray[np.int_]",
+    ) -> ValuationStatus:
+        if len(values) == 0:
+            raise ValueError("Empty values array")
+        if len(values) != len(variances) or len(values) != len(counts):
+            raise ValueError("Mismatching array lengths")
+
+        sat_ratio = np.count_nonzero(
+            np.sqrt(variances / counts) <= np.abs(eps * values)
+        ) / len(values)
+        if np.all(counts > 10) and sat_ratio >= values_ratio:
+            return ValuationStatus.Converged
+        return ValuationStatus.Pending
+
+    return check_stderr
 
 
 @lru_cache
-def combinatorial_coefficient(n: int, ns: int) -> int:
+def combinatorial_coefficient(n: int, ns: int) -> float:
     # Correction coming from Monte Carlo integration so that the mean of the
     # marginals converges to the value: the uniform distribution over the
-    # powerset of a set with n-1 elements has mass 2^{n-1} over each subset. The
-    # additional factor n corresponds to the one in the Shapley definition
+    # powerset of a set with n-1 elements has mass 2^{n-1} over each subset.
+    # The factor 1 / n corresponds to the one in the Shapley definition
     return 2 ** (n - 1) / (n * math.comb(n - 1, ns))
 
 
 @lru_cache
-def banzhaf_coefficient(n: int, _: int):
-    return 1.0 / 2 ** (n - 1)
+def banzhaf_coefficient(n: int, _: int) -> float:
+    return n / 2 ** (n - 1)
+
+
+def permutation_coefficient(_: int, __: int) -> float:
+    return 1.0
 
 
 def beta_coefficient(alpha: int, beta: int):
     @lru_cache
-    def coefficient_w_tilde(n: int, ns: int):
+    def coefficient_w_tilde(n: int, ns: int) -> float:
         j = ns + 1
         p1 = (beta + np.arange(0, j - 1)).prod() * (alpha + np.arange(0, n - j)).prod()
         p2 = (alpha + beta + np.arange(0, n - 1)).prod()
@@ -138,35 +182,37 @@ def beta_coefficient(alpha: int, beta: int):
     return coefficient_w_tilde
 
 
-def shapley(u: Utility, max_subsets: int):
-    sampler = UniformSampler(u.data.indices, max_subsets=max_subsets)
-    return _semivalues(sampler, u, combinatorial_coefficient)
+def shapley(u: Utility, criterion: StoppingCriterion):
+    sampler = UniformSampler(u.data.indices)
+    return _semivalues(u, sampler, combinatorial_coefficient, criterion)
 
 
-def permutation_shapley(u: Utility, max_subsets: int):
-    sampler = PermutationSampler(u.data.indices, max_subsets=max_subsets)
-    return _semivalues(sampler, u)
+def permutation_shapley(u: Utility, criterion: StoppingCriterion):
+    sampler = PermutationSampler(u.data.indices)
+    return _semivalues(u, sampler, permutation_coefficient, criterion)
 
 
 def beta_shapley(
-    u: Utility, alpha: int, beta: int, max_subsets: int
+    u: Utility, criterion: StoppingCriterion, alpha: int, beta: int
 ) -> ValuationResult:
-    sampler = PermutationSampler(u.data.indices, max_subsets=max_subsets)
-    return _semivalues(sampler, u, beta_coefficient(alpha, beta))
+    sampler = PermutationSampler(u.data.indices)
+    return _semivalues(u, sampler, beta_coefficient(alpha, beta), criterion)
 
 
 def beta_shapley_paper(
-    u: Utility, alpha: int, beta: int, max_subsets: int
+    u: Utility, criterion: StoppingCriterion, alpha: int, beta: int
 ) -> ValuationResult:
-    sampler = UniformSampler(u.data.indices, max_subsets=max_subsets)
-    return _semivalues(sampler, u, beta_coefficient(alpha, beta))
+    sampler = UniformSampler(u.data.indices)
+    return _semivalues(u, sampler, beta_coefficient(alpha, beta), criterion)
 
 
-def banzhaf_index(u: Utility, max_subsets: int):
-    sampler = PermutationSampler(u.data.indices, max_subsets=max_subsets)
-    return _semivalues(sampler, u)
+def banzhaf_index(u: Utility, criterion: StoppingCriterion):
+    sampler = PermutationSampler(u.data.indices)
+    return _semivalues(u, sampler, banzhaf_coefficient, criterion)
 
 
-def owen_shapley(u: Utility, max_subsets: int, max_q: int):
-    sampler = OwenSampler(u.data.indices, max_subsets=max_subsets)
+def owen_shapley(u: Utility, criterion: StoppingCriterion, max_q: int):
+    sampler = OwenSampler(
+        u.data.indices, method=OwenSampler.Algorithm.Standard, n_steps=200
+    )
     raise NotImplementedError("to do")
