7653ebf4b4c4d676d84c2d54696564f7461d995e
Miguel de Benito Delgado <m.debenito@appliedai.de>
2023-01-08 20:44:09 +0100

    More lenient test, do not run in parallel

--- tests/value/shapley/test_gt.py
+++ tests/value/shapley/test_gt.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pydvl.value import ValuationStatus
 from pydvl.value.shapley.gt import group_testing_shapley
 
 from .. import check_values
@@ -7,13 +8,14 @@ from .. import check_values
 
 @pytest.mark.parametrize(
     "num_samples, fun, atol, max_iterations, kwargs",
-    [(5, group_testing_shapley, 0.01, int(1e5), {"eps": 0.1})],
+    [(5, group_testing_shapley, 0.05, int(1e5), {"eps": 0.05})],
 )
 def test_group_testing_shapley(
     num_samples, analytic_shapley, fun, atol, max_iterations, kwargs
 ):
     u, exact_values = analytic_shapley
     values = fun(
-        u, max_iterations=int(max_iterations), progress=False, n_jobs=10, **kwargs
+        u, max_iterations=int(max_iterations), progress=False, n_jobs=1, **kwargs
     )
+    assert values.status == ValuationStatus.Converged
     check_values(values, exact_values, atol=atol)
