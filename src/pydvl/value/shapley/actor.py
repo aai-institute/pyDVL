a3d3720200526e3979cf2ab45c8ebfb2f6b5354a
Miguel de Benito Delgado <m.debenito@appliedai.de>
2023-01-19 20:00:31 +0100

    More consistent return values

--- src/pydvl/value/shapley/actor.py
+++ src/pydvl/value/shapley/actor.py
@@ -14,14 +14,14 @@ from typing import Optional, cast
 
 import numpy as np
 
-from .types import PermutationBreaker
-from ..convergence import ConvergenceCheck
 from ...utils.config import ParallelConfig
 from ...utils.parallel.actor import Coordinator, RayActorWrapper, Worker
 from ...utils.parallel.backend import RayParallelBackend, init_parallel_backend
 from ...utils.status import Status
 from ...utils.utility import Utility
 from ...value.results import ValuationResult
+from ..convergence import ConvergenceCheck, max_iterations
+from .types import PermutationBreaker
 
 __all__ = ["get_shapley_coordinator", "get_shapley_worker"]
 
@@ -71,7 +71,7 @@ class ShapleyCoordinator(Coordinator):
         super().__init__()
         self.convergence_check = convergence_check
 
-    def accumulate(self) -> Optional[ValuationResult]:
+    def accumulate(self) -> ValuationResult:
         """Accumulates all results received from the workers.
 
         :return: Values and standard errors in a
@@ -79,10 +79,10 @@ class ShapleyCoordinator(Coordinator):
             reported yet, returns ``None``.
         """
         if len(self.worker_results) == 0:
-            return None
+            return ValuationResult.empty()
 
-        # FIXME: very inefficient
-        totals = reduce(operator.add, self.worker_results)
+        # FIXME: inefficient, possibly unstable
+        totals: ValuationResult = reduce(operator.add, self.worker_results)
         # Avoid recomputing
         self.worker_results = [totals]
         return totals
