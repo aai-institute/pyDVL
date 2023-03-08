76a41854fb3ce8323051cae456750277599fa9da
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-03-08 18:37:54 +0100

    Test least core with and without non_negative_subsidy, remove extra test cases

--- tests/value/least_core/test_naive.py
+++ tests/value/least_core/test_naive.py
@@ -1,4 +1,5 @@
 import pytest
+from numpy.testing import assert_almost_equal
 
 from pydvl.value.least_core import exact_least_core
 from tests.value import check_total_value, check_values
@@ -9,17 +10,24 @@ from tests.value import check_total_value, check_values
     [
         ("miner", {"n_miners": 3}),
         ("miner", {"n_miners": 4}),
-        ("miner", {"n_miners": 8}),
-        ("miner", {"n_miners": 9}),
         ("gloves", {"left": 1, "right": 1}),
         ("gloves", {"left": 2, "right": 1}),
         ("gloves", {"left": 1, "right": 2}),
-        ("gloves", {"left": 3, "right": 1}),
     ],
     indirect=True,
 )
-def test_naive_least_core(test_utility):
+@pytest.mark.parametrize("non_negative_subsidy", (True, False))
+def test_naive_least_core(test_utility, non_negative_subsidy):
     u, exact_values = test_utility
-    values = exact_least_core(u, progress=False)
+    values = exact_least_core(
+        u, non_negative_subsidy=non_negative_subsidy, progress=False
+    )
     check_total_value(u, values)
-    check_values(values, exact_values, extra_values_names=["subsidy"])
+    if non_negative_subsidy:
+        check_values(values, exact_values)
+        # Sometimes the subsidy is negative but really close to zero
+        # Due to numerical errors
+        if values.subsidy < 0:
+            assert_almost_equal(values.subsidy, 0.0, decimal=5)
+    else:
+        check_values(values, exact_values, extra_values_names=["subsidy"])
