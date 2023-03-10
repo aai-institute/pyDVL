2a7388f3b22f49c3e1d87ab29d1d00dea5f48b84
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-03-10 15:46:24 +0100

    Add tests for passing kwargs to Dataset class methods

--- tests/utils/test_dataset.py
+++ tests/utils/test_dataset.py
@@ -27,10 +27,13 @@ def test_creating_dataset_subsclassfrom_sklearn(train_size):
     assert len(dataset) == int(train_size * len(data.data))
 
 
-def test_creating_dataset_from_x_y_arrays(train_size):
+@pytest.mark.parametrize("kwargs", ({}, {"description": "Test Dataset"}))
+def test_creating_dataset_from_x_y_arrays(train_size, kwargs):
     X, y = make_classification()
-    dataset = Dataset.from_arrays(X, y, train_size=train_size)
+    dataset = Dataset.from_arrays(X, y, train_size=train_size, **kwargs)
     assert len(dataset) == int(train_size * len(X))
+    for k, v in kwargs.items():
+        assert getattr(dataset, k) == v
 
 
 def test_creating_grouped_dataset_from_sklearn(train_size):
@@ -62,13 +65,16 @@ def test_creating_grouped_dataset_subsclassfrom_sklearn(train_size):
     assert len(dataset) == n_groups
 
 
-def test_creating_grouped_dataset_from_x_y_arrays(train_size):
+@pytest.mark.parametrize("kwargs", ({}, {"description": "Test Dataset"}))
+def test_creating_grouped_dataset_from_x_y_arrays(train_size, kwargs):
     X, y = make_classification()
     data_groups = np.random.randint(
         low=0, high=3, size=int(train_size * len(X))
     ).flatten()
     n_groups = len(np.unique(data_groups))
     dataset = GroupedDataset.from_arrays(
-        X, y, data_groups=data_groups, train_size=train_size
+        X, y, data_groups=data_groups, train_size=train_size, **kwargs
     )
     assert len(dataset) == n_groups
+    for k, v in kwargs.items():
+        assert getattr(dataset, k) == v
