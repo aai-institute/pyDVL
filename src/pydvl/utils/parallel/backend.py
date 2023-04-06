64dcde6902bb492384c65b7fc7ee019b40a1edf3
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-04-06 12:09:35 +0200

    Revert changes to wrap()
    Co-authored-by: Miguel de Benito Delgado <m.debenito@appliedai-institute.de>

--- src/pydvl/utils/parallel/backend.py
+++ src/pydvl/utils/parallel/backend.py
@@ -171,13 +171,9 @@ class RayParallelBackend(BaseParallelBackend, backend_name="ray"):
 
         :return: The `.remote` method of the ray `RemoteFunction`.
         """
-        remote_fn = ray.remote(fun)
-        if len(kwargs) == 0:
-            kwargs["num_cpus"] = self.n_cpus_per_job
-        else:
-            if "num_cpus" not in kwargs:
-                kwargs["num_cpus"] = self.n_cpus_per_job
-        return remote_fn.remote  # type: ignore
+        if len(kwargs) > 1:
+            return ray.remote(**kwargs)(fun).remote  # type: ignore
+        return ray.remote(fun).remote  # type: ignore
 
     def wait(
         self,
