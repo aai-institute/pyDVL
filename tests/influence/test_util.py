dbf74e74b8c97140b9bca8610ff9fa7a7868a683
Kristof Schröder <kristof_schroeder@web.de>
2023-06-16 21:10:31 +0200

    Improve readability of test_utils

--- tests/influence/test_util.py
+++ tests/influence/test_util.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, astuple
 from typing import Tuple
 
 import pytest
@@ -13,46 +13,50 @@ from tests.influence.conftest import linear_model, linear_hessian_analytical, li
 
 
 @dataclass
+class ModelParams:
+    dimension: Tuple[int, int]
+    condition_number: float
+    train_size: int
+
+
+@dataclass
 class UtilTestParameters:
     """
     Helper class to add more test parameter combinations
     """
-    dimension: Tuple[int, int]
-    condition_number: float
-    train_size: int
+    model_params: ModelParams
     batch_size: int
     rank_estimate: int
     regularization: float
 
 
 test_parameters = [
-    UtilTestParameters(dimension=(30, 16), condition_number=4, train_size=60, batch_size=4, rank_estimate=50,
+    UtilTestParameters(ModelParams(dimension=(30, 16), condition_number=4, train_size=60),
+                       batch_size=4,
+                       rank_estimate=50,
                        regularization=0.0001),
-    UtilTestParameters(dimension=(32, 35), condition_number=1e6, train_size=100, batch_size=5, rank_estimate=70,
+    UtilTestParameters(ModelParams(dimension=(32, 35), condition_number=1e6, train_size=100),
+                       batch_size=5,
+                       rank_estimate=70,
                        regularization=0.00001),
-    UtilTestParameters(dimension=(25, 15), condition_number=1e3, train_size=90, batch_size=10, rank_estimate=50,
+    UtilTestParameters(ModelParams(dimension=(25, 15), condition_number=1e3, train_size=90),
+                       batch_size=10,
+                       rank_estimate=50,
                        regularization=0.000001),
-    UtilTestParameters(dimension=(30, 15), condition_number=1e4, train_size=120, batch_size=8, rank_estimate=60,
+    UtilTestParameters(ModelParams(dimension=(30, 15), condition_number=1e4, train_size=120),
+                       batch_size=8,
+                       rank_estimate=60,
                        regularization=0.00001),
-    UtilTestParameters(dimension=(40, 13), condition_number=1e5, train_size=900, batch_size=4, rank_estimate=90,
+    UtilTestParameters(ModelParams(dimension=(40, 13), condition_number=1e5, train_size=900),
+                       batch_size=4,
+                       rank_estimate=90,
                        regularization=0.00001),
 ]
 
 
-dimensions = [(30, 16), (32, 35), (25, 15), (30, 15), (40, 13)]
-condition_numbers = [4, 1e6, 1e3, 1e4, 1e5]
-train_sizes = [60, 100, 90, 120, 900]
-batch_sizes = [4, 5, 10, 8, 4]
-rank_estimates = [50, 70, 50, 60, 90]
-regularizations = [0.0001, 0.00001, 0.000001, 0.00001, 0.00001]
-
-
-problem_params = [(tp.dimension, tp.condition_number, tp.train_size) for tp in test_parameters]
-
-
-@pytest.fixture(params=problem_params)
+@pytest.fixture
 def model_data(request):
-    dimension, condition_number, train_size = request.param  # type: ignore
+    dimension, condition_number, train_size = request.param
     A, b = linear_model(dimension, condition_number)
     x = np.random.uniform(size=[train_size, dimension[-1]])
     y = np.random.uniform(size=[train_size, dimension[0]])
@@ -68,7 +72,7 @@ def model_data(request):
 
 
 @pytest.mark.parametrize("model_data, tol",
-                         [(problem_param, 1e-12) for problem_param in problem_params],
+                         [(astuple(tp.model_params), 1e-12) for tp in test_parameters],
                          indirect=["model_data"])
 def test_hvp(model_data, tol: float):
     torch_model, x, y, vec, H_analytical = model_data
@@ -78,7 +82,7 @@ def test_hvp(model_data, tol: float):
 
 @pytest.mark.parametrize("use_avg, tol", [(True, 1e-3), (False, 1e-6)], ids=["avg", "full"])
 @pytest.mark.parametrize("model_data, batch_size",
-                         list(zip(problem_params, batch_sizes)),
+                         [(astuple(tp.model_params), tp.batch_size) for tp in test_parameters],
                          indirect=["model_data"])
 def test_get_hvp_function(model_data, tol: float, use_avg: bool, batch_size: int):
     torch_model, x, y, vec, H_analytical = model_data
@@ -89,7 +93,7 @@ def test_get_hvp_function(model_data, tol: float, use_avg: bool, batch_size: int
 
 
 @pytest.mark.parametrize("model_data, batch_size, rank_estimate, regularization",
-                         list(zip(problem_params, batch_sizes, rank_estimates, regularizations)),
+                         [astuple(tp) for tp in test_parameters],
                          indirect=["model_data"])
 def test_lanzcos_low_rank_hessian_approx(model_data, batch_size: int, rank_estimate, regularization):
     _, _, _, vec, H_analytical = model_data
