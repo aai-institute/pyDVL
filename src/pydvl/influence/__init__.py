ea086a563b804e3d2ca48b8d06ef51d771a11c20
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-08-10 16:45:21 +0200

    Get influence subpackage from develop branch

--- src/pydvl/influence/__init__.py
+++ src/pydvl/influence/__init__.py
@@ -1,10 +1,11 @@
 """
 This package contains algorithms for the computation of the influence function.
 
-!!! Warning
-    Much of the code in this package is experimental or untested and is subject
-    to modification. In particular, the package structure and basic API will
-    probably change.
+.. warning::
+   Much of the code in this package is experimental or untested and is subject
+   to modification. In particular, the package structure and basic API will
+   probably change.
 
 """
-from .general import *
+from .frameworks import TorchTwiceDifferentiable, TwiceDifferentiable
+from .general import compute_influence_factors, compute_influences
--- src/pydvl/influence/frameworks/__init__.py
+++ src/pydvl/influence/frameworks/__init__.py
@@ -1,5 +1,57 @@
-from .torch_differentiable import *
+# FIXME the following code was part of an attempt to accommodate different
+# frameworks. In its current form it is ugly and thus it will likely be changed
+# in the future.
 
-__all__ = [
-    "TorchTwiceDifferentiable",
-]
+import logging
+
+from .twice_differentiable import InverseHvpResult, TwiceDifferentiable
+
+__all__ = ["TwiceDifferentiable"]
+logger = logging.getLogger("frameworks")
+
+try:
+    import torch
+
+    from .torch_differentiable import TorchTwiceDifferentiable
+
+    __all__.append("TorchTwiceDifferentiable")
+
+    from .torch_differentiable import (
+        as_tensor,
+        cat,
+        einsum,
+        mvp,
+        solve_batch_cg,
+        solve_linear,
+        solve_lissa,
+        stack,
+        transpose_tensor,
+        zero_tensor,
+    )
+
+    TensorType = torch.Tensor
+    DataLoaderType = torch.utils.data.DataLoader
+    ModelType = torch.nn.Module
+
+    __all__.extend(
+        [
+            "TensorType",
+            "ModelType",
+            "InverseHvpResult",
+            "solve_linear",
+            "solve_batch_cg",
+            "solve_lissa",
+            "as_tensor",
+            "stack",
+            "cat",
+            "zero_tensor",
+            "transpose_tensor",
+            "einsum",
+            "mvp",
+        ]
+    )
+
+except ImportError:
+    logger.info(
+        "No compatible framework found. Influence function computation disabled."
+    )
