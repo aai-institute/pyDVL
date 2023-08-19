548b4158a1ee2fbfbe6db5017b6b6fa1e13c3314
Miguel de Benito Delgado <m.debenito@appliedai.de>
2023-08-19 14:48:20 +0200

    Need for speed 🐌🏁: Only test valuation methods with joblib

--- tests/value/conftest.py
+++ tests/value/conftest.py
@@ -123,16 +123,6 @@ def linear_shapley(linear_dataset, scorer, n_jobs):
     return u, exact_values
 
 
-@pytest.fixture(scope="module", params=["joblib", "ray-local", "ray-external"])
-def parallel_config(request):
-    if "ray" not in request.param:
-        yield ParallelConfig(backend=request.param, wait_timeout=0.1)
-    elif request.param == "ray-local":
-        yield ParallelConfig(backend="ray", wait_timeout=0.1)
-        ray.shutdown()
-    elif request.param == "ray-external":
-        # Starts a head-node for the cluster.
-        cluster = Cluster(initialize_head=True, head_node_args={"num_cpus": 4})
-        yield ParallelConfig(backend="ray", address=cluster.address, wait_timeout=0.1)
-        ray.shutdown()
-        cluster.shutdown()
+@pytest.fixture(scope="module")
+def parallel_config(num_workers):
+    yield ParallelConfig(backend="joblib", n_cpus_local=num_workers, wait_timeout=0.1)
