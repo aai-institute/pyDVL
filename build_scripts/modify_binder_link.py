5b57a2d2afb0ffc26787eeab0cec47d7550411e3
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-08-30 19:09:01 +0200

    Remove binder link caption

--- build_scripts/modify_binder_link.py
+++ build_scripts/modify_binder_link.py
@@ -3,6 +3,8 @@ This mkdocs hook replaces the binder link in the rendered notebooks
 with links to the actual notebooks in the repository.
 This is needed because for the docs we create symlinks to the notebooks
 inside the docs directory.
+This is heavily inspired from:
+https://github.com/greenape/mknotebooks/blob/master/mknotebooks/plugin.py#L322
 """
 
 import logging
@@ -20,7 +22,11 @@ if TYPE_CHECKING:
 logger = logging.getLogger("mkdocs")
 
 BINDER_BASE_URL = "https://mybinder.org/v2"
-BINDER_URL_PATTERN = re.compile(r"\(" + re.escape(BINDER_BASE_URL) + r".*\)")
+BINDER_LOGO_WITH_CAPTION = "[![Binder](https://mybinder.org/badge_logo.svg)]"
+BINDER_LOGO_WITHOUT_CAPTION = "[![](https://mybinder.org/badge_logo.svg)]"
+BINDER_LINK_PATTERN = re.compile(
+    re.escape(BINDER_LOGO_WITH_CAPTION) + r"\(" + re.escape(BINDER_BASE_URL) + r".*\)"
+)
 
 branch_name: Optional[str] = None
 
@@ -51,7 +57,9 @@ def on_page_markdown(
     notebook_filename = Path(page.file.src_path).name
     file_path = (notebooks_dir / notebook_filename).relative_to(root_dir)
     url_path = f"%2Ftree%2F{file_path}"
-    binder_url = f"({BINDER_BASE_URL}/gh/{repo_name}/{branch_name}?urlpath={url_path})"
-    logger.debug(f"New binder url: {binder_url}")
-    markdown = re.sub(BINDER_URL_PATTERN, binder_url, markdown)
+    binder_url = f"{BINDER_BASE_URL}/gh/{repo_name}/{branch_name}?urlpath={url_path}"
+    binder_link = f"{BINDER_LOGO_WITHOUT_CAPTION}({binder_url})"
+    logger.info(f"New binder url: {binder_url}")
+    logger.info(f"Using regex: {BINDER_LINK_PATTERN}")
+    markdown = re.sub(BINDER_LINK_PATTERN, binder_link, markdown)
     return markdown
