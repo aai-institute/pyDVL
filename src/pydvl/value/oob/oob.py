ed2db411869e05397f016c045ed09d40cbfb91ca
BastienZim <bastien.zimmermann@imt-atlantique.net>
2023-09-07 16:38:23 +0200

    Converted doctrings to mkdocs and google style ; added support for regression models ; added more parameters for the bagging function ; added a generic loss function parameter

--- src/pydvl/value/oob/oob.py
+++ src/pydvl/value/oob/oob.py
@@ -1,8 +1,10 @@
-# Code written by @BastienZim
-# Bastien Zimmermann - bastien.zimmermann@craft.ai
-# Code implementation of data_oob from Kwon and Zou "Data-OOB: Out-of-bag Estimate as a Simple and Efficient Data Value" ICML 2023
-# https://proceedings.mlr.press/v202/kwon23e.html
+"""
+## References
 
+[^1]: <a name="kwon_data_2023"></a>Kwon et al. [Data-OOB: Out-of-bag Estimate as a Simple and Efficient Data Value]
+(https://proceedings.mlr.press/v202/kwon23e.html). In: Published at ICML 2023
+
+"""
 
 from __future__ import annotations
 
@@ -11,47 +13,129 @@ from pydvl.utils import Utility, maybe_progress
 from pydvl.utils.status import Status
 from pydvl.value.result import ValuationResult
 import numpy as np
-from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier, BaggingRegressor
+from sklearn.base import is_classifier, is_regressor
+
 
 __all__ = ["compute_data_oob"]
 
 
 def compute_data_oob(
-    u: Utility, n_est: int = 10, max_samples: float = 0.8, *, progress: bool = True
+    u: Utility,
+    n_est: int = 10,
+    max_samples: float = 0.8,
+    n_jobs: int = None,
+    loss: function = None,
+    *,
+    progress: bool = True,
 ) -> ValuationResult:
     r"""Computes Data out of bag values
 
-    There is a need to tune n_est and max_samples jointly to ensure all samples are at least 1 time oob, otherwise the result could includ a nan value for that datum.
+    This implements the method described in (Kwon and Zou, 2023) <sup><a href="kwon_data_2023">1</a></sup>.
+    It fits several base estimators provided through u.model through a bagging process. The point value corresponds to the average loss of estimators which were not fit on it.
 
-    :param u: Utility object with model, data, and scoring function
-    :param n_est: Number of estimator used in the bagging procedure.
-    :param progress: If True, display a progress bar
-    :return: Object with the data values.
+    $w_{bj}\in Z$ is the number of times the j-th datum $(x_j, y_j)$ is selected in the b-th bootstrap dataset.
+
+    $$\psi((x_i,y_i),\Theta_B):=\frac{\sum_{b=1}^{B}\mathbb{1}(w_{bi}=0)T(y_i, \hat{f}_b(x_i))}{\sum_{b=1}^{B}
+    \mathbb{1}
+    (w_{bi}=0)}$$
+
+    With:
+
+    $$
+    T: Y \times Y
+    \rightarrow \mathbb{R}
+    $$
+
+    T is a score function that represents the goodness of a weak learner $\hat{f}_b$ at the i-th datum $(x_i, y_i)$.
+
+    There is a need to tune n_est and max_samples jointly to ensure all samples are at least 1 time oob, otherwise the result could include a nan value for that datum.
+
+    Args:
+        u: Utility object with model, data, and scoring function.
+        n_est: Number of estimator used in the bagging procedure.
+        max_samples: The fraction of samples to draw to train each base estimator.
+        n_jobs: The number of jobs to run in parallel used in the bagging procedure for both fit and predict.
+        loss: A function taking as parameters model prediction and corresponding data labels(preds, y) and returning an array of point wise errors.
+        progress: If True, display a progress bar.
+
+    Returns:
+        Object with the data values.
     """
 
-    n_samples = len(u.data)
-    all_counts, all_means, all_M2 = (
-        np.zeros(n_samples),
-        np.zeros(n_samples),
-        np.zeros(n_samples),
+    result = ValuationResult.empty(
+        algorithm="data_oob", indices=u.data.indices, data_names=u.data.data_names
     )
+    # check model type:
+    if is_classifier(u.model):
+        model_type = "classifier"
+    elif is_regressor(u.model):
+        model_type = "regressor"
+    else:
+        raise Exception(
+            "Model has to be classifier or a regressor in a sklearn format."
+        )
 
-    bag = BaggingClassifier(u.model, max_samples=max_samples, n_estimators=n_est)
+    if model_type == "classifier":
+        bag = BaggingClassifier(
+            u.model, n_estimators=n_est, max_samples=max_samples, n_jobs=n_jobs
+        )
+        if loss is None:
+            loss = point_wise_accuracy
+    else:
+        bag = BaggingRegressor(
+            u.model, n_estimators=n_est, max_samples=max_samples, n_jobs=n_jobs
+        )
+        if loss is None:
+            loss = neg_l2_distance
+
     bag.fit(u.data.x_train, u.data.y_train)
-    #    print(bag.score(u.data.x_test, u.data.y_test))
+
     for est, samples in maybe_progress(
         zip(bag.estimators_, bag.estimators_samples_), progress, total=n_est
     ):  # The bottleneck is the bag fitting not this part so TQDM is not very useful here
-        oob_idx = np.intersect1d(u.data.indices, np.unique(samples))
-        all_counts[oob_idx] += 1
-        accuracies = np.array(
-            est.predict(u.data.x_train[oob_idx]) == u.data.y_train[oob_idx],
-            dtype=u.data.indices.dtype,
+        oob_idx = np.setxor1d(u.data.indices, np.unique(samples))
+        array_loss = loss(
+            preds=est.predict(u.data.x_train[oob_idx]), y=u.data.y_train[oob_idx]
         )
         result += ValuationResult(
             algorithm="data_oob",
             indices=oob_idx,
-            values=accuracies,
-            counts=np.ones_like(accuracies),
+            values=array_loss,
+            counts=np.ones_like(array_loss, dtype=u.data.indices.dtype),
         )
     return result
+
+
+def point_wise_accuracy(preds: np.array, y: np.array):
+    r"""Computes point wise accuracy
+
+    Args:
+        preds: Model prediction on
+        y:  data labels corresponding to the model predictions
+
+    Returns:
+        Array of point wise accuracy
+    """
+    return np.array(
+        preds == y,
+        dtype=bool,
+    )
+
+
+def neg_l2_distance(preds: np.array, y: np.array):
+    r"""Computes negative l2 distance between label and model prediction
+
+    Args:
+        preds: Model prediction on
+        y:  data labels corresponding to the model predictions
+
+    Returns:
+        Array with point wise negative l2 distance between label and model prediction
+    """
+    return np.square(
+        np.array(
+            preds - y,
+            dtype=np.float64,
+        )
+    )
