4118505b24f668f09b2fd67a7aae1d2713e719b0
Miguel de Benito Delgado <m.debenito@appliedai.de>
2023-09-17 12:19:53 +0200

    Improve deprecation warnings

--- src/pydvl/value/semivalues.py
+++ src/pydvl/value/semivalues.py
@@ -173,6 +173,14 @@ def _marginal(
 #     deprecated_in="0.8.0",
 #     remove_in="0.9.0",
 # )
+@deprecated(
+    target=True,
+    deprecated_in="0.7.0",
+    remove_in="0.9.0",
+    args_mapping={"batch_size": None},
+    template_mgs="batch_size is for experimental use and will be removed"
+    "in future versions.",
+)
 def compute_generic_semivalues(
     sampler: PowersetSampler,
     u: Utility,
@@ -216,8 +224,8 @@ def compute_generic_semivalues(
 
     if batch_size != 1:
         warnings.warn(
-            "Parameter `batch_size` is for experimental use and will be removed in "
-            "future versions",
+            "Parameter `batch_size` is for experimental use and will be"
+            " removed in future versions",
             DeprecationWarning,
         )
 
@@ -304,6 +312,14 @@ def beta_coefficient(alpha: float, beta: float) -> SVCoefficient:
     return cast(SVCoefficient, beta_coefficient_w)
 
 
+@deprecated(
+    target=True,
+    deprecated_in="0.7.0",
+    remove_in="0.9.0",
+    args_mapping={"batch_size": None},
+    template_mgs="batch_size is for experimental use and will be removed"
+    "in future versions.",
+)
 def compute_shapley_semivalues(
     u: Utility,
     *,
@@ -355,6 +371,14 @@ def compute_shapley_semivalues(
     )
 
 
+@deprecated(
+    target=True,
+    deprecated_in="0.7.0",
+    remove_in="0.9.0",
+    args_mapping={"batch_size": None},
+    template_mgs="batch_size is for experimental use and will be removed"
+    "in future versions.",
+)
 def compute_banzhaf_semivalues(
     u: Utility,
     *,
@@ -404,6 +428,14 @@ def compute_banzhaf_semivalues(
     )
 
 
+@deprecated(
+    target=True,
+    deprecated_in="0.7.0",
+    remove_in="0.9.0",
+    args_mapping={"batch_size": None},
+    template_mgs="batch_size is for experimental use and will be removed"
+    "in future versions.",
+)
 def compute_beta_shapley_semivalues(
     u: Utility,
     *,
@@ -456,11 +488,7 @@ def compute_beta_shapley_semivalues(
     )
 
 
-@deprecated(
-    target=True,
-    deprecated_in="0.7.0",
-    remove_in="0.8.0",
-)
+@deprecated(target=True, deprecated_in="0.7.0", remove_in="0.8.0")
 class SemiValueMode(str, Enum):
     """Enumeration of semi-value modes.
 
