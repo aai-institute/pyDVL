55ab99b5a923f8edac87541b03a6eb9e854e4142
Kristof Schröder <kristof_schroeder@web.de>
2023-11-19 15:36:34 +0100

    Refactor influence.torch.influence_model: * add docstrings * inline method for lissa to not call deprecated method * add optional input for factor and value methods

--- src/pydvl/influence/torch/influence_model.py
+++ src/pydvl/influence/torch/influence_model.py
@@ -7,9 +7,11 @@ import torch
 from torch import nn as nn
 from torch.utils.data import DataLoader
 
+from ...utils import maybe_progress
 from ..inversion import InfluenceRegistry, InversionMethod
 from ..twice_differentiable import Influence, InfluenceType, InverseHvpResult
 from .functional import (
+    get_batch_hvp,
     get_hessian,
     matrix_jacobian_product,
     per_sample_gradient,
@@ -23,7 +25,7 @@ from .torch_differentiable import (
     solve_batch_cg,
     solve_lissa,
 )
-from .util import flatten_dimensions, to_model_device
+from .util import flatten_dimensions
 
 logger = logging.getLogger(__name__)
 
@@ -75,10 +77,44 @@ class TorchInfluence(Influence[torch.Tensor], ABC):
         self,
         x_test: torch.Tensor,
         y_test: torch.Tensor,
-        x: torch.Tensor,
-        y: torch.Tensor,
-        influence_type: InfluenceType,
+        x: Optional[torch.Tensor] = None,
+        y: Optional[torch.Tensor] = None,
+        influence_type: InfluenceType = InfluenceType.Up,
     ) -> InverseHvpResult:
+
+        if x is None:
+
+            if y is not None:
+                raise ValueError(
+                    "Providing labels y, without providing model input x is not supported"
+                )
+
+            return self._symmetric_values(
+                x_test.to(self.model_device),
+                y_test.to(self.model_device),
+                influence_type,
+            )
+
+        if y is None:
+            raise ValueError()  # TODO: error message
+
+        return self._non_symmetric_values(
+            x_test.to(self.model_device),
+            y_test.to(self.model_device),
+            x.to(self.model_device),
+            y.to(self.model_device),
+            influence_type,
+        )
+
+    def _non_symmetric_values(
+        self,
+        x_test: torch.Tensor,
+        y_test: Optional[torch.Tensor] = None,
+        x: Optional[torch.Tensor] = None,
+        y: Optional[torch.Tensor] = None,
+        influence_type: InfluenceType = InfluenceType.Up,
+    ):
+
         if influence_type is InfluenceType.Up:
             if x_test.shape[0] <= y.shape[0]:
                 factor, info = self.factors(x_test, y_test)
@@ -91,22 +127,49 @@ class TorchInfluence(Influence[torch.Tensor], ABC):
             values = self.perturbation(factor, x, y)
         return InverseHvpResult(values, info)
 
+    def _symmetric_values(
+        self, x: torch.Tensor, y: torch.Tensor, influence_type: InfluenceType
+    ) -> InverseHvpResult[torch.Tensor]:
+
+        grad = self._loss_grad(x, y)
+        fac, info = self._solve_hvp(grad)
+
+        if influence_type is InfluenceType.Up:
+            values = fac @ grad.T
+        else:
+            values = self.perturbation(fac, x, y)
+
+        return InverseHvpResult(values, info)
+
     def up_weighting(
         self,
         z_test_factors: torch.Tensor,
         x: torch.Tensor,
         y: torch.Tensor,
     ) -> torch.Tensor:
-        return z_test_factors @ self._loss_grad(x, y).T
+
+        return (
+            z_test_factors
+            @ self._loss_grad(x.to(self.model_device), y.to(self.model_device)).T
+        )
 
     def perturbation(
-        self, z_test_factors: torch.Tensor, x: torch.Tensor, y: torch.Tensor
+        self,
+        z_test_factors: torch.Tensor,
+        x: torch.Tensor,
+        y: torch.Tensor,
     ) -> torch.Tensor:
+
         return torch.einsum(
-            "ia,jba->ijb", z_test_factors, self._flat_loss_mixed_grad(x, y)
+            "ia,jba->ijb",
+            z_test_factors,
+            self._flat_loss_mixed_grad(
+                x.to(self.model_device), y.to(self.model_device)
+            ),
         )
 
     def factors(self, x: torch.Tensor, y: torch.Tensor) -> InverseHvpResult:
+
         return self._solve_hvp(
             self._loss_grad(x.to(self.model_device), y.to(self.model_device))
         )
@@ -221,6 +284,31 @@ class BatchCgInfluence(TorchInfluence):
 
 
 class LissaInfluence(TorchInfluence):
+    """
+    Uses LISSA, Linear time Stochastic Second-Order Algorithm, to iteratively
+    approximate the inverse Hessian. More precisely, it finds x s.t. \(Hx = b\),
+    with \(H\) being the model's second derivative wrt. the parameters.
+    This is done with the update
+
+    \[H^{-1}_{j+1} b = b + (I - d) \ H - \frac{H^{-1}_j b}{s},\]
+
+    where \(I\) is the identity matrix, \(d\) is a dampening term and \(s\) a scaling
+    factor that are applied to help convergence. For details, see
+    (Koh and Liang, 2017)<sup><a href="#koh_liang_2017">1</a></sup> and the original paper
+    (Agarwal et. al.)<sup><a href="#agarwal_secondorder_2017">2</a></sup>.
+
+    Args:
+        model: instance of [torch.nn.Module][torch.nn.Module].
+        training_data: A DataLoader containing the training data.
+        hessian_perturbation: Regularization of the hessian.
+        maxiter: Maximum number of iterations.
+        dampen: Dampening factor, defaults to 0 for no dampening.
+        scale: Scaling factor, defaults to 10.
+        h0: Initial guess for hvp.
+        rtol: tolerance to use for early stopping
+        progress: If True, display progress bars.
+    """
+
     def __init__(
         self,
         model: nn.Module,
@@ -245,20 +333,61 @@ class LissaInfluence(TorchInfluence):
         self.train_dataloader = train_dataloader
 
     def _solve_hvp(self, rhs: torch.Tensor) -> InverseHvpResult:
-        # TODO directly implement the method here and remove call to obsolete function
-        x, info = solve_lissa(
-            TorchTwiceDifferentiable(self.model, self.loss),
-            self.train_dataloader,
-            rhs,
-            self.hessian_regularization,
-            maxiter=self.maxiter,
-            dampen=self.dampen,
-            scale=self.scale,
-            h0=self.h0,
-            rtol=self.rtol,
-            progress=self.progress,
+
+        h_estimate = self.h0 if self.h0 is not None else torch.clone(rhs)
+
+        shuffled_training_data = DataLoader(
+            self.train_dataloader.dataset,
+            self.train_dataloader.batch_size,
+            shuffle=True,
         )
-        return InverseHvpResult(x, info)
+
+        def lissa_step(
+            h: torch.Tensor, reg_hvp: Callable[[torch.Tensor], torch.Tensor]
+        ) -> torch.Tensor:
+            """Given an estimate of the hessian inverse and the regularised hessian
+            vector product, it computes the next estimate.
+
+            Args:
+                h: An estimate of the hessian inverse.
+                reg_hvp: Regularised hessian vector product.
+
+            Returns:
+                The next estimate of the hessian inverse.
+            """
+            return rhs + (1 - self.dampen) * h - reg_hvp(h) / self.scale
+
+        model_params = {
+            k: p.detach() for k, p in self.model.named_parameters() if p.requires_grad
+        }
+        b_hvp = torch.vmap(
+            get_batch_hvp(self.model, self.loss), in_dims=(None, None, None, 0)
+        )
+        for _ in maybe_progress(range(self.maxiter), self.progress, desc="Lissa"):
+            x, y = next(iter(shuffled_training_data))
+            # grad_xy = model.grad(x, y, create_graph=True)
+            reg_hvp = (
+                lambda v: b_hvp(model_params, x, y, v) + self.hessian_regularization * v
+            )
+            residual = lissa_step(h_estimate, reg_hvp) - h_estimate
+            h_estimate += residual
+            if torch.isnan(h_estimate).any():
+                raise RuntimeError("NaNs in h_estimate. Increase scale or dampening.")
+            max_residual = torch.max(torch.abs(residual / h_estimate))
+            if max_residual < self.rtol:
+                break
+
+        mean_residual = torch.mean(torch.abs(residual / h_estimate))
+
+        logger.info(
+            f"Terminated Lissa with {max_residual*100:.2f} % max residual."
+            f" Mean residual: {mean_residual*100:.5f} %"
+        )
+        info = {
+            "max_perc_residual": max_residual * 100,
+            "mean_perc_residual": mean_residual * 100,
+        }
+        return InverseHvpResult(x=h_estimate / self.scale, info=info)
 
 
 class ArnoldiInfluence(TorchInfluence):
@@ -294,11 +423,11 @@ class ArnoldiInfluence(TorchInfluence):
                 eigen_computation_on_gpu=eigen_computation_on_gpu,
             )
 
-        self.low_rank_representation = to_model_device(low_rank_representation, model)
+        super().__init__(model, loss)
+        self.low_rank_representation = low_rank_representation.to(self.model_device)
         self.return_low_rank_representation_in_info = (
             return_low_rank_representation_in_info
         )
-        super().__init__(model, loss)
 
     def prepare_for_distributed(self) -> "Influence":
         if self.return_low_rank_representation_in_info:
@@ -309,14 +438,14 @@ class ArnoldiInfluence(TorchInfluence):
             )
         return self
 
-    def values(
+    def _non_symmetric_values(
         self,
         x_test: torch.Tensor,
-        y_test: torch.Tensor,
-        x: torch.Tensor,
-        y: torch.Tensor,
-        influence_type: InfluenceType,
-    ) -> InverseHvpResult:
+        y_test: Optional[torch.Tensor] = None,
+        x: Optional[torch.Tensor] = None,
+        y: Optional[torch.Tensor] = None,
+        influence_type: InfluenceType = InfluenceType.Up,
+    ) -> InverseHvpResult[torch.Tensor]:
 
         if influence_type is InfluenceType.Up:
             mjp = matrix_jacobian_product(
@@ -335,7 +464,28 @@ class ArnoldiInfluence(TorchInfluence):
             info["low_rank_representation"] = self.low_rank_representation
         return InverseHvpResult(values, info)
 
+    def _symmetric_values(
+        self, x: torch.Tensor, y: torch.Tensor, influence_type: InfluenceType
+    ) -> InverseHvpResult[torch.Tensor]:
+
+        if influence_type is InfluenceType.Up:
+            left = matrix_jacobian_product(
+                self.model, self.loss, self.low_rank_representation.projections.T
+            )(self.model_params, x, y)
+            right = (
+                torch.diag_embed(1.0 / self.low_rank_representation.eigen_vals) @ left
+            )
+            values = torch.einsum("ij, ik -> jk", left, right)
+        else:
+            factors, _ = self.factors(x, y)
+            values = self.perturbation(factors, x, y)
+        info = {}
+        if self.return_low_rank_representation_in_info:
+            info["low_rank_representation"] = self.low_rank_representation
+        return InverseHvpResult(values, info)
+
     def _solve_hvp(self, rhs: torch.Tensor) -> InverseHvpResult:
+        # TODO directly implement the method here and remove call to obsolete function
         x, info = solve_arnoldi(
             TorchTwiceDifferentiable(self.model, self.loss),
             None,
