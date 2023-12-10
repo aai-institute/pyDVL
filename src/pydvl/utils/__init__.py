efca4238661ee25da05c30a1b7373ab09eebcb86
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-12-10 12:37:12 +0100

    Delete progress module

--- src/pydvl/utils/__init__.py
+++ src/pydvl/utils/__init__.py
@@ -3,7 +3,6 @@ from .caching import *
 from .config import *
 from .dataset import *
 from .numeric import *
-from .progress import *
 from .score import *
 from .status import *
 from .types import *
