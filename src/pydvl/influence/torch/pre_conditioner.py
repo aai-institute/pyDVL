0780903ea2fae99371c28f57a42a378bfd9641d7
Kristof Schroeder <kristof_schroeder@web.de>
2024-03-20 10:36:06 +0100

    Refactor pre-conditioners: * simplify dimensionality handling in NystroemPreConditioner * add and extend docstrings

--- src/pydvl/influence/torch/pre_conditioner.py
+++ src/pydvl/influence/torch/pre_conditioner.py
@@ -6,8 +6,21 @@ import torch
 from ..base_influence_function_model import NotFittedException
 from .functional import LowRankProductRepresentation, randomized_nystroem_approximation
 
+__all__ = ["JacobiPreConditioner", "NystroemPreConditioner", "PreConditioner"]
+
 
 class PreConditioner(ABC):
+    r"""
+    Abstract base class for implementing pre-conditioners for improving the convergence
+    of CG for systems of the form
+
+    \[ ( A + \lambda \operatorname{I})x = \operatorname{rhs} \]
+
+    i.e. a matrix $M$ such that $M^{-1}(A + \lambda \operatorname{I})$ has a better
+    condition number than $A + \lambda \operatorname{I}$.
+
+    """
+
     @property
     @abstractmethod
     def is_fitted(self):
@@ -22,10 +35,32 @@ class PreConditioner(ABC):
         device: torch.device,
         regularization: float = 0.0,
     ):
+        r"""
+        Implement this to fit the pre-conditioner to the matrix represented by the
+        mat_mat_prod
+        Args:
+            mat_mat_prod: a callable that computes the matrix-matrix product
+            size: size of the matrix represented by `mat_mat_prod`
+            dtype: data type of the matrix represented by `mat_mat_prod`
+            device: device of the matrix represented by `mat_mat_prod`
+            regularization: regularization parameter $\lambda$ in the equation
+                $ ( A + \lambda \operatorname{I})x = \operatorname{rhs} $
+        Returns:
+            self
+        """
         pass
 
     def solve(self, rhs: torch.Tensor):
+        r"""
+        Solve the equation $M@Z = \operatorname{rhs}$
+        Args:
+            rhs: right hand side of the equation, corresponds to the residuum vector
+                (or matrix) in the conjugate gradient method
 
+        Returns:
+            solution $M^{-1}\operatorname{rhs}$
+
+        """
         if not self.is_fitted:
             raise NotFittedException(type(self))
 
@@ -40,15 +75,19 @@ class JacobiPreConditioner(PreConditioner):
     r"""
     Pre-conditioner for improving the convergence of CG for systems of the form
 
-    \[ ( A + \lambda \operatorname{I})x = \operatorname{rhs} \]
+    $$ ( A + \lambda \operatorname{I})x = \operatorname{rhs} $$
 
     The JacobiPreConditioner uses the diagonal information of the matrix $A$.
     The diagonal elements are not computed directly but estimated via Hutchinson's
-    estimator,
+    estimator.
 
-    \[ \frac{1}{m} \sum_{i=1}^m u_i \odot Au_i\]
+    $$ M = \frac{1}{m} \sum_{i=1}^m u_i \odot Au_i + \lambda \operatorname{I} $$
 
-    Works well in the case the matrix \(A\) is diagonal dominant.
+    where $u_i$ are i.i.d. Gaussian random vectors.
+    Works well in the case the matrix $A + \lambda \operatorname{I}$ is diagonal
+    dominant.
+    For more information, see the documentation of
+    [Conjugate Gradient][conjugate-gradient]
     Args:
         num_samples_estimator: number of samples to use in computation of
             Hutchinson's estimator
@@ -73,8 +112,8 @@ class JacobiPreConditioner(PreConditioner):
         regularization: float = 0.0,
     ):
         r"""
-        Computing an estimate of the diagonal of the matrix represented by
-        the mat_mat_prod via Hutchinson's estimator
+        Fits by computing an estimate of the diagonal of the matrix represented by
+        `mat_mat_prod` via Hutchinson's estimator
 
         Args:
             mat_mat_prod: a callable representing the matrix-matrix product
@@ -82,7 +121,7 @@ class JacobiPreConditioner(PreConditioner):
             dtype: needed data type of inputs for the mat_mat_prod
             device: needed device for inputs of mat_mat_prod
             regularization: regularization parameter
-                \(\lambda\) in \((A+\lambda I)x=b\)
+                $\lambda$ in $(A+\lambda I)x=b$
         """
         random_samples = torch.randn(
             size, self.num_samples_estimator, device=device, dtype=dtype
@@ -107,17 +146,19 @@ class NystroemPreConditioner(PreConditioner):
     r"""
     Pre-conditioner for improving the convergence of CG for systems of the form
 
-    \[ (A + \lambda \operatorname{I})x = \operatorname{rhs} \]
+    $$ (A + \lambda \operatorname{I})x = \operatorname{rhs} $$
 
     The NystroemPreConditioner computes a low-rank approximation
 
-    \[ A_{\text{nys}} = (A \Omega)(\Omega^T A \Omega)^{+}(A \Omega)^T
-    = U \Sigma U^T \]
+    $$ A_{\text{nys}} = (A \Omega)(\Omega^T A \Omega)^{\dagger}(A \Omega)^T
+    = U \Sigma U^T, $$
 
+    where $(\cdot)^{\dagger}$ denotes the [Moore-Penrose inverse](
+    https://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_inverse),
     and uses the matrix
 
-    \[ P^{-1} = (\lambda + \sigma_{\text{rank}})U(\Sigma+
-        \lambda \operatorname{I})^{-1}U^T+(\operatorname{I} - UU^T) \]
+    $$ M^{-1} = (\lambda + \sigma_{\text{rank}})U(\Sigma+
+        \lambda \operatorname{I})^{-1}U^T+(\operatorname{I} - UU^T) $$
 
     for pre-conditioning, where \(  \sigma_{\text{rank}} \) is the smallest
     eigenvalue of the low-rank approximation.
@@ -150,8 +191,8 @@ class NystroemPreConditioner(PreConditioner):
         regularization: float = 0.0,
     ):
         r"""
-        Computing a low-rank approximation of the matrix represented by `mat_mat_prod`
-        via Nystroem approximation
+        Fits by computing a low-rank approximation of the matrix represented by
+        `mat_mat_prod` via Nystroem approximation
 
         Args:
             mat_mat_prod: a callable representing the matrix-matrix product
@@ -159,7 +200,7 @@ class NystroemPreConditioner(PreConditioner):
             dtype: needed data type of inputs for the mat_mat_prod
             device: needed device for inputs of mat_mat_prod
             regularization: regularization parameter
-                \(\lambda\) in \((A+\lambda I)x=b\)
+                $\lambda$  in $(A+\lambda I)x=b$
         """
 
         self._low_rank_approx = randomized_nystroem_approximation(
@@ -168,14 +209,17 @@ class NystroemPreConditioner(PreConditioner):
         self._regularization = regularization
 
     def _solve(self, rhs: torch.Tensor):
+
+        rhs_is_one_dim = rhs.ndim == 1
+
+        rhs_view = torch.atleast_2d(rhs).t() if rhs_is_one_dim else rhs
+
         regularized_eigenvalues = (
             self._low_rank_approx.eigen_vals + self._regularization
         )
         lambda_rank = self._low_rank_approx.eigen_vals[-1] + self._regularization
 
-        proj_rhs = self._low_rank_approx.projections.t() @ (
-            rhs.unsqueeze(-1) if rhs.ndim == 1 else rhs
-        )
+        proj_rhs = self._low_rank_approx.projections.t() @ rhs_view
 
         inverse_regularized_eigenvalues = lambda_rank / regularized_eigenvalues
 
@@ -183,7 +227,9 @@ class NystroemPreConditioner(PreConditioner):
             proj_rhs * inverse_regularized_eigenvalues.unsqueeze(-1)
         )
 
-        result += (
-            rhs.unsqueeze(-1) if rhs.ndim == 1 else rhs
-        ) - self._low_rank_approx.projections @ proj_rhs
-        return result.squeeze()
+        result += rhs_view - self._low_rank_approx.projections @ proj_rhs
+
+        if rhs_is_one_dim:
+            result = result.squeeze()
+
+        return result
