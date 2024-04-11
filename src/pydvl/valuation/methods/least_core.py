c01a92dc879be15413815c10f40f973383c98065
Janos Gabler <janos.gabler@gmail.com>
2024-04-11 10:13:04 +0200

    Write tests for new interface and implement a dummy LeastCoreValuation to make them pass.

--- src/pydvl/valuation/methods/least_core.py
+++ src/pydvl/valuation/methods/least_core.py
@@ -1,28 +1,77 @@
 from __future__ import annotations
 
+from enum import Enum
+
 from pydvl.valuation.base import Valuation
 from pydvl.valuation.dataset import Dataset
 from pydvl.valuation.samplers.powerset import PowersetSampler
 from pydvl.valuation.utility.base import UtilityBase
 
+# Imports from old value folder
+from pydvl.value.least_core.montecarlo import *
+from pydvl.value.least_core.naive import *
+
 __all__ = ["LeastCoreValuation"]
+
+
+class LeastCoreMode(Enum):
+    """Available Least Core algorithms."""
+
+    MonteCarlo = "montecarlo"
+    Exact = "exact"
 
 
 class LeastCoreValuation(Valuation):
     def __init__(
         self,
         utility: UtilityBase,
-        sampler: PowersetSampler,
-        n_constraints: int,
-        non_negative_subsidy: float,
+        n_jobs: int = 1,
+        n_iterations: int | None = None,
+        # sampler: PowersetSampler,
+        mode: LeastCoreMode = LeastCoreMode.MonteCarlo,
+        non_negative_subsidy: bool = False,
         solver_options: dict | None = None,
+        progress: bool = False,
+        **kwargs,
     ):
         super().__init__()
         self.utility = utility
-        self.sampler = sampler
-        self.n_constraints = n_constraints
+        self.n_jobs = n_jobs
+        self.n_iterations = n_iterations
+        # self.sampler = sampler
+        self.mode = mode
         self.non_negative_subsidy = non_negative_subsidy
         self.solver_options = solver_options
+        self.progress = progress
+        self.kwargs = kwargs
 
     def fit(self, data: Dataset) -> None:
-        pass
+
+        if self.mode == LeastCoreMode.MonteCarlo:
+            # TODO fix progress showing in remote case
+            self.progress = False
+            if self.n_iterations is None:
+                raise ValueError(
+                    "n_iterations cannot be None for Monte Carlo Least Core"
+                )
+            values = montecarlo_least_core(  # type: ignore
+                u=self.utility,
+                n_iterations=self.n_iterations,
+                n_jobs=self.n_jobs,
+                progress=self.progress,
+                non_negative_subsidy=self.non_negative_subsidy,
+                solver_options=self.solver_options,
+                **self.kwargs,
+            )
+        elif self.mode == LeastCoreMode.Exact:
+            values = exact_least_core(
+                u=self.utility,
+                progress=self.progress,
+                non_negative_subsidy=self.non_negative_subsidy,
+                solver_options=self.solver_options,
+            )
+
+        else:
+            raise ValueError(f"Invalid value encountered in {mode=}")
+
+        self.result = values
---
+++ tests/valuation/methods/test_least_core.py
@@ -0,0 +1,76 @@
+import logging
+
+import pytest
+from numpy.testing import assert_almost_equal
+
+from pydvl.valuation.methods.least_core import LeastCoreMode, LeastCoreValuation
+from tests.valuation import check_total_value, check_values
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize(
+    "test_game, n_iterations",
+    [
+        (("miner", {"n_players": 8}), 128),
+        (("shoes", {"left": 10, "right": 5}), 10000),
+    ],
+    indirect=["test_game"],
+)
+@pytest.mark.parametrize("n_jobs", [1, -1])
+@pytest.mark.parametrize("non_negative_subsidy", (True, False))
+def test_montecarlo_least_core(
+    test_game, n_iterations, n_jobs, non_negative_subsidy, seed
+):
+    valuation = LeastCoreValuation(
+        utility=test_game.u,
+        n_jobs=n_jobs,
+        n_iterations=n_iterations,
+        non_negative_subsidy=non_negative_subsidy,
+        progress=False,
+        seed=seed,
+    )
+    valuation.fit(data=test_game.u.data)
+    values = valuation.result
+    exact_values = test_game.least_core_values()
+    if non_negative_subsidy:
+        check_values(values, exact_values)
+        # Sometimes the subsidy is negative but really close to zero
+        # Due to numerical errors
+        if values.subsidy < 0:
+            assert_almost_equal(values.subsidy, 0.0, decimal=5)
+    else:
+        check_values(values, exact_values, extra_values_names=["subsidy"])
+
+
+@pytest.mark.parametrize(
+    "test_game",
+    [
+        ("miner", {"n_players": 3}),
+        ("miner", {"n_players": 4}),
+        ("shoes", {"left": 1, "right": 1}),
+        ("shoes", {"left": 2, "right": 1}),
+        ("shoes", {"left": 1, "right": 2}),
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize("non_negative_subsidy", (True, False))
+def test_naive_least_core(test_game, non_negative_subsidy):
+    valuation = LeastCoreValuation(
+        utility=test_game.u,
+        non_negative_subsidy=non_negative_subsidy,
+        progress=False,
+        mode=LeastCoreMode.Exact,
+    )
+    valuation.fit(data=test_game.u.data)
+    values = valuation.result
+    check_total_value(test_game.u, values)
+    exact_values = test_game.least_core_values()
+    if non_negative_subsidy:
+        check_values(values, exact_values)
+        # Sometimes the subsidy is negative but really close to zero
+        # Due to numerical errors
+        if values.subsidy < 0:
+            assert_almost_equal(values.subsidy, 0.0, decimal=5)
+    else:
+        check_values(values, exact_values, extra_values_names=["subsidy"])
