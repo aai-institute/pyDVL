aae6b3b74b21df6e5d6c01b7bc00f2dba7087f9d
Miguel de Benito Delgado <m.debenito@appliedai.de>
2024-11-13 14:04:25 +0100

    Move legacy code outside core dist

--- notebooks/support/legacy.py
+++ notebooks/support/legacy.py
@@ -4,21 +4,18 @@ from typing import Iterable
 
 import numpy as np
 from numpy.typing import NDArray
-from tqdm.auto import tqdm
+from tqdm import tqdm
 
-from pydvl.valuation.dataset import Dataset
-from pydvl.valuation.result import ValuationResult
-from pydvl.valuation.types import Sample
-from pydvl.valuation.utility.modelutility import ModelUtility
+from pydvl.utils import Utility
+from pydvl.value.result import ValuationResult as LegacyValuationResult
 
-__all__ = ["compute_removal_score"]
+__all__ = ["compute_removal_score_legacy"]
 
 
-def compute_removal_score(
-    u: ModelUtility,
-    values: ValuationResult,
-    training_data: Dataset,
-    percentages: NDArray[np.float_] | Iterable[float],
+def compute_removal_score_legacy(
+    u: Utility,
+    values: LegacyValuationResult,
+    percentages: NDArray[np.float64] | Iterable[float],
     *,
     remove_best: bool = False,
     progress: bool = False,
@@ -36,15 +33,14 @@ def compute_removal_score(
     Returns:
         Dictionary that maps the percentages to their respective scores.
     """
-    u = u.with_dataset(training_data)
 
     # Sanity checks
     if np.any([x >= 1.0 or x < 0.0 for x in percentages]):
         raise ValueError("All percentages should be in the range [0.0, 1.0)")
 
-    if len(values) != len(training_data.indices):
+    if len(values) != len(u.data.indices):
         raise ValueError(
-            f"The number of values, {len(values) }, should be equal to the number of data indices, {len(training_data.indices)}"
+            f"The number of values, {len(values) }, should be equal to the number of data indices, {len(u.data.indices)}"
         )
 
     scores = {}
@@ -53,8 +49,8 @@ def compute_removal_score(
     values.sort(reverse=remove_best)
 
     for pct in tqdm(percentages, disable=not progress, desc="Removal Scores"):
-        n_removal = int(pct * len(training_data))
+        n_removal = int(pct * len(u.data))
         indices = values.indices[n_removal:]
-        score = u(Sample(idx=None, subset=indices))
+        score = u(indices)
         scores[pct] = score
     return scores
