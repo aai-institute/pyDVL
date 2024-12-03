5438901f84e04c0e35afbdb039a7ead5cf49202d
Miguel de Benito Delgado <m.debenito@appliedai.de>
2024-12-03 11:43:02 +0100

    Add tests for valuation.dataset

--- tests/valuation/test_dataset.py
+++ tests/valuation/test_dataset.py
@@ -2,8 +2,8 @@ import numpy as np
 import pytest
 from sklearn.datasets import load_wine, make_classification
 
-from pydvl.utils.dataset import Dataset, GroupedDataset
-from pydvl.value.result import ValuationResult
+from pydvl.valuation.dataset import Dataset, GroupedDataset
+from pydvl.valuation.result import ValuationResult
 
 
 @pytest.fixture(scope="module", params=[0.1, 0.5, 0.8])
@@ -13,8 +13,8 @@ def train_size(request):
 
 def test_creating_dataset_from_sklearn(train_size):
     data = load_wine()
-    dataset = Dataset.from_sklearn(data, train_size=train_size)
-    assert len(dataset) == int(train_size * len(data.data))
+    train, test = Dataset.from_sklearn(data, train_size=train_size)
+    assert len(train) == int(train_size * len(data.data))
 
 
 def test_creating_dataset_subsclassfrom_sklearn(train_size):
@@ -23,28 +23,31 @@ def test_creating_dataset_subsclassfrom_sklearn(train_size):
     class TestDataset(Dataset):
         ...
 
-    dataset = TestDataset.from_sklearn(data, train_size=train_size)
-    assert isinstance(dataset, TestDataset)
-    assert len(dataset) == int(train_size * len(data.data))
+    train, test = TestDataset.from_sklearn(data, train_size=train_size)
+    assert isinstance(train, TestDataset)
+    assert isinstance(test, TestDataset)
+    assert len(train) == int(train_size * len(data.data))
 
 
 @pytest.mark.parametrize("kwargs", ({}, {"description": "Test Dataset"}))
 def test_creating_dataset_from_x_y_arrays(train_size, kwargs):
     X, y = make_classification()
-    dataset = Dataset.from_arrays(X, y, train_size=train_size, **kwargs)
-    assert len(dataset) == int(train_size * len(X))
+    train, test = Dataset.from_arrays(X, y, train_size=train_size, **kwargs)
+    assert len(train) == int(train_size * len(X))
     for k, v in kwargs.items():
-        assert getattr(dataset, k) == v
+        assert getattr(train, k) == v
 
 
 def test_creating_grouped_dataset_from_sklearn(train_size):
     data = load_wine()
     data_groups = np.random.randint(low=0, high=3, size=len(data.data)).flatten()
     n_groups = len(np.unique(data_groups))
-    dataset = GroupedDataset.from_sklearn(
+    train, test = GroupedDataset.from_sklearn(
         data, data_groups=data_groups, train_size=train_size
     )
-    assert len(dataset) == n_groups
+    assert len(train) <= n_groups
+    assert len(test) <= n_groups
+    assert len(train.get_data()[0]) == int(train_size * len(data.data))
 
 
 def test_creating_grouped_dataset_from_sklearn_failure(train_size):
@@ -66,11 +69,13 @@ def test_creating_grouped_dataset_subsclassfrom_sklearn(train_size):
 
     data_groups = np.random.randint(low=0, high=3, size=len(data.data)).flatten()
     n_groups = len(np.unique(data_groups))
-    dataset = TestGroupedDataset.from_sklearn(
+    train, test = TestGroupedDataset.from_sklearn(
         data, data_groups=data_groups, train_size=train_size
     )
-    assert isinstance(dataset, TestGroupedDataset)
-    assert len(dataset) == n_groups
+    assert isinstance(train, TestGroupedDataset)
+    assert len(train) <= n_groups
+    assert len(test) <= n_groups
+    assert len(train.get_data()[0]) == int(train_size * len(data.data))
 
 
 @pytest.mark.parametrize("kwargs", ({}, {"description": "Test Dataset"}))
@@ -78,12 +83,14 @@ def test_creating_grouped_dataset_from_x_y_arrays(train_size, kwargs):
     X, y = make_classification()
     data_groups = np.random.randint(low=0, high=3, size=len(X)).flatten()
     n_groups = len(np.unique(data_groups))
-    dataset = GroupedDataset.from_arrays(
+    train, test = GroupedDataset.from_arrays(
         X, y, data_groups=data_groups, train_size=train_size, **kwargs
     )
-    assert len(dataset) == n_groups
+    assert len(train) <= n_groups
+    assert len(test) <= n_groups
+    assert len(train.get_data()[0]) == int(train_size * len(X))
     for k, v in kwargs.items():
-        assert getattr(dataset, k) == v
+        assert getattr(train, k) == v
 
 
 def test_creating_grouped_dataset_from_x_y_arrays_failure(train_size):
@@ -102,18 +109,18 @@ def test_grouped_dataset_results():
     X, y = make_classification()
     train_size = 0.5
     data_groups = np.random.randint(low=0, high=3, size=len(X)).flatten()
-    dataset = GroupedDataset.from_arrays(
+    train, test = GroupedDataset.from_arrays(
         X, y, data_groups=data_groups, train_size=train_size
     )
 
-    v = ValuationResult.zeros(indices=dataset.indices, data_names=dataset.data_names)
+    v = ValuationResult.zeros(indices=train.indices, data_names=train.data_names)
     v2 = ValuationResult(
-        indices=dataset.indices,
-        values=np.ones(len(dataset)),
-        variances=np.zeros(len(dataset)),
-        data_names=dataset.data_names,
+        indices=train.indices,
+        values=np.ones(len(train)),
+        variances=np.zeros(len(train)),
+        data_names=train.data_names,
     )
     v += v2
     assert np.all(v.values == 1)
-    assert np.all(v.names == np.array(dataset.data_names))
+    assert np.all(v.names == np.array(train.data_names))
     assert np.all([isinstance(x, np.int_) for x in v.names])
