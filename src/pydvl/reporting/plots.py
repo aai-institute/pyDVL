cec61f7e446391558f63b20aa409a09d7abd0453
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2025-01-16 19:39:09 +0100

    Fix plot to use new valuationresult

--- src/pydvl/reporting/plots.py
+++ src/pydvl/reporting/plots.py
@@ -276,7 +276,10 @@ def plot_shapley(
     if ax is None:
         _, ax = plt.subplots()
 
-    yerr = norm.ppf(1 - level / 2) * df[f"{prefix}_stderr"]
+    stderr = np.sqrt(
+        df[f"{prefix}_variances"] / np.maximum(1.0, df[f"{prefix}_counts"])
+    )
+    yerr = norm.ppf(1 - level / 2) / stderr
 
     ax.errorbar(x=df.index, y=df[prefix], yerr=yerr, fmt="o", capsize=6)
     ax.set_xlabel(xlabel or "")
