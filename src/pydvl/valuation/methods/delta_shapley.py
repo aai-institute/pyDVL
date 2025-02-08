0bd30dd7f699cd3c040a08374adeaf7d5a59d871
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2025-02-08 13:52:50 +0100

    Improve doc for delta shapley, and fix a problem

--- src/pydvl/valuation/methods/delta_shapley.py
+++ src/pydvl/valuation/methods/delta_shapley.py
@@ -1,6 +1,28 @@
-from __future__ import annotations
+r"""
+This module implements the $\delta$-Shapley valuation method.
 
-import math
+The value of a point $i$ is defined as:
+
+$$
+v_\delta(i) = \sum_{k=l}^u w(k) \sum_{S \subset D_{-i}^{(k)}} [U(S_{+i}) - U(S)],
+$$
+
+where $l$ and $u$ are the lower and upper bounds of the size of the subsets to sample
+from, and $w(k)$ is the weight of a subset of size $k$ in the complement of $\{i\}$, and
+is given by:
+
+$$
+\begin{array}{ll}
+w (k) = \left \{
+    \begin{array}{ll}
+        \frac{1}{u - l + 1} & \text{if} l \ \leq k \leq u,\\ 0 &
+        \text{otherwise.}
+    \end{array} \right. &
+    \end{array}
+$$
+"""
+
+from __future__ import annotations
 
 from pydvl.utils.types import Seed
 from pydvl.valuation.methods.semivalue import SemivalueValuation
@@ -15,14 +37,21 @@ from pydvl.valuation.utility.base import UtilityBase
 class DeltaShapleyValuation(SemivalueValuation):
     r"""Computes $\delta$-Shapley values.
 
-    $\delta$-Shapley does not accept custom samplers. Instead it uses a truncated
-    hierarchical powerset sampler with a lower and upper bound on the size of the sets
-    to sample from.
+    $\delta$-Shapley does not accept custom samplers. Instead, it uses a
+    [TruncatedUniformStratifiedSampler][pydvl.valuation.samplers.TruncatedUniformStratifiedSampler]
+    with a lower and upper bound on the size of the sets to sample from.
 
-    TODO See ...
+    Args:
+        utility: Object to compute utilities.
+        is_done: Stopping criterion to use.
+        lower_bound: The lower bound of the size of the subsets to sample from.
+        upper_bound: The upper bound of the size of the subsets to sample from.
+        seed: The seed for the random number generator used by the sampler.
+        progress: Whether to show a progress bar
     """
 
     algorithm_name = "Delta-Shapley"
+    sampler: TruncatedUniformStratifiedSampler
 
     def __init__(
         self,
@@ -37,9 +66,12 @@ class DeltaShapleyValuation(SemivalueValuation):
             lower_bound=lower_bound, upper_bound=upper_bound, seed=seed
         )
         super().__init__(utility, sampler, is_done, progress=progress)
-        raise NotImplementedError(
-            "Delta-Shapley has not been properly implemented nor tested yet."
-        )
 
     def coefficient(self, n: int, k: int, weight: float) -> float:
-        return weight / math.comb(n - 1, k)
+        assert self.sampler.lower_bound is not None
+        assert self.sampler.upper_bound is not None
+
+        assert self.sampler.lower_bound <= k <= self.sampler.upper_bound, (
+            "Invalid subset size"
+        )
+        return weight / (self.sampler.upper_bound - self.sampler.lower_bound + 1)
