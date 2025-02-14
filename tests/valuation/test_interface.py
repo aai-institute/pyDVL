9b29d2f7099423d4d1d6465e33efa83dae75211a
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2025-02-14 20:04:55 +0100

    Missing param after change to truncation defaults

--- tests/valuation/test_interface.py
+++ tests/valuation/test_interface.py
@@ -53,7 +53,9 @@ def test_loo_valuation(train_data, utility, n_jobs):
 def test_shapley_valuation(train_data, utility, n_jobs):
     valuation = ShapleyValuation(
         utility,
-        sampler=PermutationSampler(DeviationTruncation(burn_in_fraction=0.1)),
+        sampler=PermutationSampler(
+            DeviationTruncation(burn_in_fraction=0.1, sigmas=1.0)
+        ),
         is_done=MaxUpdates(5),
         progress=False,
     )
