464ca8ee3ad980ad96baae57db3131c3693aa278
Miguel de Benito Delgado <m.debenito@appliedai.de>
2025-03-09 20:03:12 +0100

    Checklist: valuation.methods

--- src/pydvl/valuation/methods/__init__.py
+++ src/pydvl/valuation/methods/__init__.py
@@ -1,8 +1,20 @@
 """
 This module contains the implementations of all valuation methods.
 
+This includes **semi-values**, **model-specific** methods and any related algorithms.
+For several of the semi-value methods there are two implementations:
+
+* A generic one combining some of the [available samplers][pydvl.valuation.samplers]
+  with a subclass of
+  [SemivalueValuation][pydvl.valuation.methods.semivalue.SemivalueValuation] which
+  defines a coefficient function.
+* A dedicated class that chooses the right sampler to avoid numerical issues when
+  computing otherwise necessary importance sampling corrections. You can read more about
+  this in [Sampling strategies for semi-values][semi-values-sampling].
+
 !!! info
-    For a full list see [here][implemented-methods-data-valuation].
+    For a full list of algorithms see: [All data valuation methods
+    implemented][implemented-methods-data-valuation].
 """
 
 from .banzhaf import *
