2a0f402c33a8d3c0e394a0c98b07d10d18995448
Miguel de Benito Delgado <m.debenito@appliedai.de>
2025-03-09 19:55:43 +0100

    Checklist: valuation.methods

--- src/pydvl/valuation/methods/__init__.py
+++ src/pydvl/valuation/methods/__init__.py
@@ -1,3 +1,10 @@
+"""
+This module contains the implementations of all valuation methods.
+
+!!! info
+    For a full list see [here][implemented-methods-data-valuation].
+"""
+
 from .banzhaf import *
 from .beta_shapley import *
 from .classwise_shapley import *
